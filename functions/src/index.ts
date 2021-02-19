472d9690f0eda2665733b962538e7379a5626438
Aomi <aomijokoji@gmail.com>
2021-02-18 18:59:35 -0800

    fix: initialize CRA app with typescript
    fix: remove used remote config template
    
    feat: add firebase related gitignores
    
    fix: linting issues

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,7 @@ import * as functions from "firebase-functions";
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
