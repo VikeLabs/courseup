5263b49d812d7233cae13d5768ef2997c465cfa0
Aomi <aomijokoji@gmail.com>
2021-02-18 21:26:41 -0800

    feat: init express app

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,15 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest(async (_request, response) => {
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+const app = express();
+
+app.get('/hello', async (req, res) => {
   functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
+  res.send('Hello from Firebase with Express!');
 });
+
+// By default, /api/* will be routed to this Express app.
+export const api = functions.https.onRequest(app);
