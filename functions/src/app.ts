f3154f52cef3521aaedc12b5f94858a8047d746d
Aomi <aomijokoji@gmail.com>
2021-02-21 17:25:42 -0800

    fix: handle routing when deployed on gcp cloud functions

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -3,7 +3,12 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { RegisterRoutes } from '../build/routes';
 
+import * as openapi from '../build/swagger.json';
+
 export const app = express();
+
+// DO NOT CHANGE UNLESS firebase.json record is updated as well.
+const API_PREFIX = 'api';
 
 // Use body parser to read sent json payloads
 app.use(
@@ -12,5 +17,20 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+
+// a slight "hack" to get things to play well in Cloud Functions
+// none of the below is required when running locally.
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.url.indexOf(`/${API_PREFIX}/`) === 0) {
+      req.url = req.url.substring(API_PREFIX.length + 1);
+    }
+    next();
+  });
+  app.get('/openapi.json', async (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(openapi);
+  });
+}
 
 RegisterRoutes(app);
