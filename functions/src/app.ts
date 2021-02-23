d7977169218c648a9f2242536c2eb083b3f15f3d
Aomi <aomijokoji@gmail.com>
2021-02-22 18:10:37 -0800

    feat: move cloudfunction specific out into app.ts

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -7,9 +7,6 @@ import * as openapi from '../build/swagger.json';
 
 export const app = express();
 
-// DO NOT CHANGE UNLESS firebase.json record is updated as well.
-const API_PREFIX = 'api';
-
 // Use body parser to read sent json payloads
 app.use(
   bodyParser.urlencoded({
@@ -18,22 +15,11 @@ app.use(
 );
 app.use(bodyParser.json());
 
-// a slight "hack" to get things to play well in Cloud Functions
-// none of the below is required when running locally.
-// Since the CDN rewrite rule in firebase.json sends all requests that start with `/api`
-// to the Cloud Function but the Express app doesn't have a route that starts
-// with `/api`, we need to remove it or make another router with app.use('/api', actualRouter)`
-if (process.env.NODE_ENV === 'production') {
-  app.use((req, res, next) => {
-    if (req.url.indexOf(`/${API_PREFIX}/`) === 0) {
-      req.url = req.url.substring(API_PREFIX.length + 1);
-    }
-    next();
-  });
-  app.get('/openapi.json', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(openapi);
-  });
-}
+// TODO: can probably accomplish the same thing using hosting.
+// serve the OpenAPI spec.
+app.get('/openapi.json', async (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(openapi);
+});
 
 RegisterRoutes(app);
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,9 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+
+// THIS FILE SHOULDN'T RUN WITHOUT FIREBASE EMULATOR!
+// nor is it meant to be. use server.ts!
+
 // FIX: need to initialize Firebase prior to import of app.
 // there's probably a better way to fix this issue.
 admin.initializeApp();
@@ -7,6 +11,23 @@ import { app } from './app';
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
+
+// DO NOT CHANGE UNLESS firebase.json record is updated as well.
+const API_PREFIX = 'api';
+
+// a slight "hack" to get things to play well in Cloud Functions
+// none of the below is required when running locally.
+// Since the CDN rewrite rule in firebase.json sends all requests that start with `/api`
+// to the Cloud Function but the Express app doesn't have a route that starts
+// with `/api`, we need to remove it or make another router with app.use('/api', actualRouter)`
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.url.indexOf(`/${API_PREFIX}/`) === 0) {
+      req.url = req.url.substring(API_PREFIX.length + 1);
+    }
+    next();
+  });
+}
 
 // By default, /api/* will be routed to this Express app.
 export const api = functions.https.onRequest(app);
