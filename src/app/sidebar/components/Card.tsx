25a15b06d49d67d988d99d89adf68026a287cc04
sunwoo1002 <chsunwoo1002@gmail.com>
2021-02-27 16:33:11 -0800

    update it card components

--- src/app/sidebar/components/Card.tsx
+++ src/app/sidebar/components/Card.tsx
@@ -1,89 +1,22 @@
-import React, {Component} from "react"
-import styled from "styled-components";
+import { Box, Heading } from "@chakra-ui/react";
+import React from "react";
 
-
-
-const Button = styled.button`
-  width: 250px;
-  height: 97px;
-  display: flex;
-  align-items: flex-start;
-  flex-direction: column;
-  padding: 20px;
-  border: none;
-  border-radius: 12px;
-  margin: 8px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  word-wrap: break-word;
-`
-const Title = styled.h1`
-  width: 110px;
-  height: 24px;
-  position: static;
-  text-align: left;
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 24px;
-  line-height: 24px;
-  color: rgba(0, 0, 0, 0.85);
-  flex: none;
-  order: 0;
-  flex-grow: 0;
-  margin: 0px 0px;
-`
-const Subtitle = styled.h2`
-  width: 192px;
-  height: 35px;
-  display: -webkit-box;
-        -webkit-line-clamp: 2; /* number of lines to show */
-        -webkit-box-orient: vertical;
-  position: static;
-  text-align: left;
-  padding: 5px;
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 12px;
-  line-height: 15px;
-  color: rgba(0, 0, 0, 0.45);
-  text-overflow: ellipsis;
-  flex: none;
-  overflow: hidden;
-`
-
-interface Inputs {
-   code: string
-   title: string
-   subject: string
-}
-interface StateVar {
-  is_white: boolean
+export interface CardProps {
+  title: string;
+  subject: string;
+  code: string;
+  selected?: boolean;
 }
 
-class Card extends Component<Inputs, StateVar> {
-  constructor(props:Inputs) {
-    super(props);
-    this.state = {
-      is_white: true
-    }
-    this.changeColor = this.changeColor.bind(this);
-  }
-
-  changeColor() {
-      this.setState({is_white: !this.state.is_white})
-  }
-  render() {
-    let bgColor = this.state.is_white ? "white" : "orange"
-    return (
-      <div>
-        <Button style={{background: bgColor}} onClick={this.changeColor}>
-          <Title>{this.props.subject} {this.props.code}</Title>
-          <Subtitle>{this.props.title}</Subtitle>
-        </Button>
-      </div>
-    );
-  }
+export function Card(props: CardProps): JSX.Element {  
+  return (
+    <Box bgColor={props.selected ? 'orange.200' : 'white'} color="black" m="5" p="4">
+      <Heading as="h3" size="md">
+        {props.subject} {props.code}
+      </Heading>
+      <Heading as="h4" color="grey" size="sm">
+        {props.title}
+      </Heading>
+    </Box>
+  );
 }
-
-export default Card
