e044b290492e3a6929781dfd1a4e4139a0a2a04b
Alex <zthh626@gmail.com>
2021-02-27 22:35:33 -0800

    CardDropDown Component

---
+++ src/app/sidebar/components/CardDropDown.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { Box, Collapse, Flex, Heading, IconButton } from "@chakra-ui/react";
+import styled from "@emotion/styled";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { Colors } from "../shared/styles";
+
+export interface CardProps {
+  title: string;
+  code: string;
+  courses: JSX.Element[];
+}
+
+export function CardDropDown(props: CardProps): JSX.Element {
+    const [isDisplayed, setDisplayed] = useState(false);
+
+    const handleClick = () => {
+        setDisplayed(!isDisplayed)
+    }
+
+    return (
+        <Flex direction="column">
+            <StyledBox>
+                <Flex direction="row"  alignItems="center" justifyContent="space-between">
+                    <Heading size="sm" color={Colors.black} fontWeight={"normal"}>
+                        {props.code} - {props.title}
+                    </Heading>
+                    <StyledIconButton
+                        onClick={handleClick}
+                        background="none"
+                        aria-label="See Courses"
+                        size="xs"
+                        icon={<ChevronDownIcon/>}
+                    />
+                </Flex>
+            </StyledBox>
+
+            <Collapse in={isDisplayed} animateOpacity>
+                {props.courses}
+            </Collapse>
+        </Flex>
+    );
+}
+
+const StyledBox = styled(Box)`
+    background-color: ${Colors.white};
+    color: ${Colors.black};
+    padding: 1em;
+    margin-top: 1em;
+`
+
+const StyledIconButton = styled(IconButton)`
+    padding: 0.5em;
+`
