f66c758757f0fc3e6be263e8f5e7129202aa35fe
Alex <zthh626@gmail.com>
2021-02-28 17:06:33 -0800

    removed shared.styles

--- src/app/sidebar/components/Card.tsx
+++ src/app/sidebar/components/Card.tsx
@@ -1,6 +1,5 @@
 import { Box, Heading } from "@chakra-ui/react";
 import React from "react";
-import { Colors } from "../shared/styles";
 
 export interface CardProps {
   title: string;
@@ -11,11 +10,11 @@ export interface CardProps {
 
 export function Card(props: CardProps): JSX.Element {  
   return (
-    <Box bgColor={props.selected ? Colors.orange200 : Colors.white} color={Colors.black} m="5" p="4">
+    <Box bgColor={props.selected ? "orange.200" : "white"} color="black" m="5" p="4">
       <Heading as="h3" size="md">
         {props.subject} {props.code}
       </Heading>
-      <Heading as="h4" color={Colors.grey} size="sm">
+      <Heading as="h4" color="grey" size="sm">
         {props.title}
       </Heading>
     </Box>
--- src/app/sidebar/shared/styles.ts
+++
@@ -1,6 +0,0 @@
-export const Colors = {
-    black: "black",
-    grey: "grey",
-    orange200: "#FBD38D",
-    white: "white"
-}
