437c461cdda1da82cb5432aa653ea96704ef5ce5
Alex <zthh626@gmail.com>
2021-02-28 21:52:34 -0800

    Sidebar Component with infinite scroller

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Center, ChakraProvider, Flex } from "@chakra-ui/react";
+import { Center, ChakraProvider, Container, Flex } from "@chakra-ui/react";
 import { Header, Content, Sidebar } from "./app/index";
 
 export function App(): JSX.Element | null {
@@ -8,9 +8,7 @@ export function App(): JSX.Element | null {
       <Flex h="100vh" direction="column">
         <Header />
         <Flex color="white" h="100%" grow={1}>
-          <Center w="300px" bg="#E4E4E4" minW="10%">
-            <Sidebar />
-          </Center>
+          <Sidebar term="202105" />
           <Center flex="1" bg="tomato" minW="80%">
             <Content />
           </Center>
--- src/app/sidebar/Sidebar.tsx
+++ src/app/sidebar/Sidebar.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
+import { Container, Flex, Heading } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { Card } from './components/Card';
+import { CardDropDown } from './components/CardDropDown';
+import { Course, Term, useGetCourses, useSubjects } from '../../fetchers'
+
 // import StyledHeader from './Header.styles';
 
 
 export interface SidebarProps {
   /**
-   * Content
-   * Subject to change
+   * Term Selected
+   * Determines what term the subjects and courses are from
    */
-  content?: string;
+  term: Term;
 }
 
-/**
- * Primary UI component for content
- */
-export const Sidebar: React.FC<SidebarProps> = ({
-  content
-}) => {
+export function Sidebar({ term }: SidebarProps): JSX.Element {
+
+  const { data: subjects, loading: loadingSubjects, error: errorSubjects } = useSubjects({ term: term });
+  const { data: courses, loading: loadingCourses, error: errorCourses } = useGetCourses({ term: term })
+
+  const parsedCourses: { [subject: string]: Course[] } = {}
+
+  if (courses != null) {
+    for (let i = 0; i < courses.length; i++) {
+      let course: Course = courses[i];
+      let subject = course.subject;
+      if (!(subject in parsedCourses)) {
+        parsedCourses[subject] = [];
+      }
+      parsedCourses[subject].push(course)
+    }
+  }
+
   return (
-    <div>
-      {content}
-    </div>
-  )
-};
+    <Flex
+      id="scrollableFlex"
+      maxH="100vh"
+      bg="#E4E4E4"
+      minW="30%"
+      p="4"
+      overflow="auto"
+    >
+      <InfiniteScroll
+        dataLength={subjects?.length ?? 0}
+        next={
+          () => { }
+        }
+        hasMore={false}
+        loader={
+          <Heading size="sm">Loading...</Heading>
+        }
+        scrollableTarget="scrollableFlex"
+        endMessage={<div />}
+      >
+        {subjects?.map((subject, index) => (
+          <CardDropDown key={index} subject={subject.subject} title={subject.title}>
+            {parsedCourses[subject.subject]?.map((course, index) => (
+              <Card key={index} title={course.title} code={course.code} subject={course.subject} />
+            ))}
+          </CardDropDown>
+        ))}
+      </InfiniteScroll>
+    </Flex>
+  );
+}
