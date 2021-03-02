f1a60a803542713fb2fc014b1139afcc6da969c1
Aomi <aomijokoji@gmail.com>
2021-03-01 19:37:32 -0800

    feat: render course content

--- src/app/content/Content.tsx
+++ src/app/content/Content.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useGetCourses } from '../../fetchers';
+import { Box, Skeleton } from '@chakra-ui/react';
+import { Term, useGetCourse } from '../../fetchers';
+import { CourseInfo } from './components/Course';
+import { SectionsContainer } from './containers/Section';
 
 export interface ContentProps {
   /**
@@ -7,18 +10,34 @@ export interface ContentProps {
    * Subject to change
    */
   content?: string;
+  pid?: string;
+  term?: Term;
 }
 
 /**
  * Primary UI component for content
  */
-export const Content: React.FC<ContentProps> = ({
-  content
-}) => {
-  const { data } = useGetCourses({ term: '202101' });
+export function Content({ pid, term = '202101' }: ContentProps): JSX.Element {
+  const { data, loading } = useGetCourse({ term, pid: pid || 'BJ2favTQ4' });
+  // const mockHours = { lecture: '1', lab: '1', tutorial: '1' };
+
   return (
-    <div>
-      {data && data.map((e) => (<p key={e.pid}>{e.title}</p>))}
-    </div>
-  )
-};
+    <Box maxWidth="1080px" bg="white" p="5">
+      <Skeleton isLoaded={!loading}>
+        {data && (
+          <>
+            <CourseInfo
+              subject={data.subject}
+              code={data.code}
+              title={data.title}
+              description={data.description || ''}
+              // hours={mockHours}
+              credits={data.credits.value}
+            />
+            <SectionsContainer term={term} subject={data.subject} code={data.code} />
+          </>
+        )}
+      </Skeleton>
+    </Box>
+  );
+}
