be83bccb6fd8b1ad0e09fa464eb2279753d70b6d
Aomi <aomijokoji@gmail.com>
2021-03-02 17:41:44 -0800

    feat: add state for pid

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import { Center, ChakraProvider, Flex, Select } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Center, ChakraProvider, Flex } from '@chakra-ui/react';
 import { Header, Content, Sidebar } from './app/index';
-import { useGetCourses } from './fetchers';
 
 export function App(): JSX.Element {
-  const { data } = useGetCourses({ term: '202101' });
-  console.log(data);
-  const [pid, setPid] = React.useState<string | undefined>();
+  const [pid, setPid] = useState<string>('');
   return (
     <ChakraProvider>
       <Flex h="100vh" direction="column">
         <Header />
         <Flex color="white" h="100%" grow={1}>
           <Center w="300px" bg="#E4E4E4" minW="10%">
-             <Sidebar term="202105" setPid={setPid} />
+            <Sidebar term="202105" setPid={setPid} pid={pid} />
           </Center>
           <Flex overflowY="auto" width="100%" justifyContent="center">
             <Content pid={pid} />
