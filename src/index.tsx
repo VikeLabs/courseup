059236883141a67bc0127c9ebbbafc68b708a8b3
Aomi <aomijokoji@gmail.com>
2021-03-08 21:55:51 -0800

    feat: point to relative URL when in prod

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <RestfulProvider base="https://staging-clockwork.vikelabs.dev/api">
+    <RestfulProvider base={process.env.NODE_ENV === 'development' ? 'https://clockwork.vikelabs.dev/api' : '/api'}>
       <App />
     </RestfulProvider>
   </React.StrictMode>,
