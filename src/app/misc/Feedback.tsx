d20ba93accaa99c2d304212470f61298f27a2728
Keith <khr8771@gmail.com>
2021-03-16 17:16:55 -0700

    modularize feedback code

--- src/app/misc/Feedback.tsx
+++ src/app/misc/Feedback.tsx
@@ -13,8 +13,28 @@ import {
   VStack,
   Image,
 } from '@chakra-ui/react';
-import { useState } from 'react';
+import { PropsWithChildren, useState } from 'react';
 import { VscFeedback, VscChevronDown } from 'react-icons/vsc';
+
+type Props = {
+  src: string;
+  href: string;
+};
+
+function FeedbackElement({ href, src, children }: PropsWithChildren<Props>): JSX.Element | null {
+  return (
+    <Link href={href} target="_empty" _hover={{ textDecoration: 'none' }}>
+      <VStack borderRadius="xl" bg="white" mt="10px" shadow="md">
+        <Center bg="blue.300" borderTopRadius="xl" maxH="60px" objectFit="cover" overflow="hidden">
+          <Image src={src} h="100%" objectFit="cover" overflow="hidden" />
+        </Center>
+        <Box px="20px" pb="20px">
+          <Text>{children}</Text>
+        </Box>
+      </VStack>
+    </Link>
+  );
+}
 
 export function Feedback(): JSX.Element | null {
   const [isOpen, setIsOpen] = useState(false);
@@ -46,35 +66,19 @@ export function Feedback(): JSX.Element | null {
             <PopoverBody textAlign="left">
               Clockwork is in the early stages of development and feedback is greatly appreciated! <br />
               Share your thoughts with the development team on the following forums:
-              <Link href="" target="_empty" _hover={{ textDecoration: 'none' }}>
-                <VStack borderRadius="xl" bg="white" mt="20px" shadow="md">
-                  <Center bg="blue.300" borderTopRadius="xl" maxH="60px" objectFit="cover" overflow="hidden">
-                    <Image src="https://growthsupermarket.com/wp-content/uploads/2018/06/GoogleForms_logo-e1529921391153.png" />
-                  </Center>
-                  <Box px="20px" pb="20px">
-                    Provide your thoughts and feedback on the site by filling out the Google Form.
-                  </Box>
-                </VStack>
-              </Link>
-              <Link
-                href="https://github.com/VikeLabs/clockwork/discussions"
-                target="_empty"
-                _hover={{ textDecoration: 'none' }}
+              <FeedbackElement
+                src="https://growthsupermarket.com/wp-content/uploads/2018/06/GoogleForms_logo-e1529921391153.png"
+                //TODO: add link once google form is made
+                href=""
               >
-                <VStack borderRadius="xl" bg="white" mt="10px" shadow="md">
-                  <Center bg="blue.300" borderTopRadius="xl" maxH="60px" objectFit="cover" overflow="hidden">
-                    <Image
-                      src="https://pngimg.com/uploads/github/github_PNG15.png"
-                      h="100%"
-                      objectFit="cover"
-                      overflow="hidden"
-                    />
-                  </Center>
-                  <Box px="20px" pb="20px">
-                    <Text>Help us fix bugs by contributing to the discussion on GitHub.</Text>
-                  </Box>
-                </VStack>
-              </Link>
+                Provide your thoughts and feedback on the site by filling out the Google Form.
+              </FeedbackElement>
+              <FeedbackElement
+                src="https://pngimg.com/uploads/github/github_PNG15.png"
+                href="https://github.com/VikeLabs/clockwork/discussions"
+              >
+                Help us fix bugs by contributing to the discussion on GitHub.
+              </FeedbackElement>
             </PopoverBody>
           </Box>
         </PopoverContent>
