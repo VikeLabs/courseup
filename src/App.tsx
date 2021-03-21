3ea13a8b5b9053a5d30ea883aacb7ea02fcd56a7
Manav Shergill <shergillmanav@uvic.ca>
2021-03-20 22:09:34 -0700

    Added routes folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Box, Center, ChakraProvider, Flex, Heading, Text, VStack } from '@chakra-ui/react';
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { TermContext } from './app/context/TermContext';
 import { Header, Content, SidebarContainer } from './app/index';
@@ -15,36 +14,26 @@ export function App(): JSX.Element | null {
       <TermContext.Provider value={{ term, setTerm }}>
         <Flex h="100vh" direction="column">
           <Header />
-          <Router>
-            <Routes>
-              <Route path="/" />
-              <Route
-                path="/calendar"
-                element={
-                  <Box grow={1} overflow="hidden" height="100%">
-                    <Flex color="white" height="100%">
-                      <SidebarContainer term={term as Term} pid={pid || ''} setPid={setPid} />
-                      <Flex minW="80%" overflow="auto" justifyContent="center" height="100%" boxShadow="lg" zIndex={56}>
-                        {pid ? (
-                          <Content term={term as Term} pid={pid} />
-                        ) : (
-                          <Center p="10">
-                            <VStack>
-                              <Heading color="black">Explore Courses</Heading>
-                              <Text color="gray">
-                                Select a subject and then a course to start viewing course details and section
-                                information.
-                              </Text>
-                            </VStack>
-                          </Center>
-                        )}
-                      </Flex>
-                    </Flex>
-                  </Box>
-                }
-              />
-            </Routes>
-          </Router>
+
+          <Box grow={1} overflow="hidden" height="100%">
+            <Flex color="white" height="100%">
+              <SidebarContainer term={term as Term} pid={pid || ''} setPid={setPid} />
+              <Flex minW="80%" overflow="auto" justifyContent="center" height="100%" boxShadow="lg" zIndex={56}>
+                {pid ? (
+                  <Content term={term as Term} pid={pid} />
+                ) : (
+                  <Center p="10">
+                    <VStack>
+                      <Heading color="black">Explore Courses</Heading>
+                      <Text color="gray">
+                        Select a subject and then a course to start viewing course details and section information.
+                      </Text>
+                    </VStack>
+                  </Center>
+                )}
+              </Flex>
+            </Flex>
+          </Box>
         </Flex>
       </TermContext.Provider>
     </ChakraProvider>
