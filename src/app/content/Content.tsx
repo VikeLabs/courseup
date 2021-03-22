307dd11df11aa721456c0a42923a1d47dff259c0
Sunwoo <chsunwoo1002@gmail.com>
2021-03-21 20:32:31 -0700

    save selected code, subject in App.tsx to enhence content component (#78)
    * save selected code, subject in App.tsx to enhence content component
    
    * relocate the SectionsContain componenet part
    
    * rdelete the lines that reset code, subject when handleTopBarClick called
    
    * delete <>

--- src/app/content/Content.tsx
+++ src/app/content/Content.tsx
@@ -19,29 +19,35 @@ export interface ContentProps {
    * Determines what term the subjects and courses are from
    */
   term: Term;
+  /**
+   * subject of selected course
+   */
+  subject: string;
+  /**
+   * code of selected course
+   */
+  code: string;
 }
 
 /**
  * Primary UI component for content
  */
-export function Content({ pid, term }: ContentProps): JSX.Element {
+export function Content({ pid, term, subject, code }: ContentProps): JSX.Element {
   const { data, loading } = useGetCourse({ term, pid });
 
   return (
     <Box maxWidth="1080px" bg="white" p="5" my="4" height="100%" boxShadow="sm" zIndex={60}>
       <Skeleton isLoaded={!loading}>
         {data && (
-          <>
-            <CourseInfo
-              subject={data.subject}
-              code={data.code}
-              title={data.title}
-              description={data.description || ''}
-              credits={data.credits.value}
-            />
-            <SectionsContainer term={term} subject={data.subject} code={data.code} />
-          </>
+          <CourseInfo
+            subject={data.subject}
+            code={data.code}
+            title={data.title}
+            description={data.description || ''}
+            credits={data.credits.value}
+          />
         )}
+        <SectionsContainer term={term} subject={subject} code={code} />
       </Skeleton>
     </Box>
   );
