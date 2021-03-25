93391cc714c8b5a2be5aeb21f35084193df880a6
Aomi <aomi@users.noreply.github.com>
2021-03-25 00:33:01 -0700

    feat: Add filtering of courses based on availibility  (#71)
    * fix: make into static functions
    
    * fix: switch to not use react port
    
    * add collection types
    
    * refactor: move into course service
    
    * feat: modify course controller to handle query param to filtering
    
    * chore: regenerate specs

--- functions/src/db/collections.ts
+++ functions/src/db/collections.ts
@@ -1,6 +1,38 @@
-import { collection as FirestoreCollection } from 'typesaurus';
-import { CourseMapping } from '../sections/Section.model';
+import { collection, subcollection, Ref } from 'typesaurus';
 
-const courseMappings = FirestoreCollection<CourseMapping>('section_mappings');
+export type CourseDoc = {
+  // human metadata
+  title: string;
+  subject: string;
+  code: string;
+  // indexed / query
+  term: string;
+  // course metadata
+  pid: string;
+  crns: string[];
+  // if crns.length > 0 then true else false.
+  inSession: boolean;
 
-export const collection = { courseMappings };
+  // datetime metadata
+  createdAt: Date;
+  updatedAt?: Date;
+};
+
+export type SectionDoc = {
+  // parent-collection ref
+  course: Ref<CourseDoc>;
+  //
+  crn: string;
+  instructors?: string[];
+};
+
+// highest level collection
+const CoursesCollection = collection<CourseDoc>('courses');
+
+// subcollection of Course
+const SectionsSubstore = subcollection<SectionDoc, CourseDoc>(
+  'sections',
+  CoursesCollection
+);
+
+export { CoursesCollection, SectionsSubstore };
