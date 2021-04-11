d347ec4e72113aa0f50cbe38ffff12b9f4305973
Keith <khr8771@gmail.com>
2021-04-11 13:04:37 -0700

    did some testing

--- src/app/header/Header.tsx
+++ src/app/header/Header.tsx
@@ -1,4 +1,8 @@
-import { Center, Grid, GridItem, Text } from '@chakra-ui/react';
+import { Button, Center, Flex, Grid, GridItem, HStack, IconButton, Spacer, Text } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
+import useWindowDimensions from '../../shared/hooks/useWindowDimensions';
 
 import { Search } from './components/SearchBar';
 import { TermButtons } from './components/TermButtons';
@@ -7,10 +11,34 @@ export interface HeaderProps {
   onSearchChange?: (query: string) => void;
   onSearchSubmit?: (query: string) => void;
 }
+
+function HeaderMobile(): JSX.Element {
+  return (
+    <HStack as="header" py="3" px="8" boxShadow="md" bg="white" zIndex={100}>
+      <Spacer />
+      <Text fontSize="x-large" fontWeight="bold">
+        clockwork
+      </Text>
+      <Spacer />
+      <IconButton aria-label="hamburger" icon={<GiHamburgerMenu />} bgColor="rgba(255, 255, 255, 0)" />
+    </HStack>
+  );
+}
+
 /**
  * Primary UI component for content
  */
 export function Header({ onSearchChange }: HeaderProps): JSX.Element {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const { width } = useWindowDimensions();
+
+  useEffect(() => {
+    if (width < 480) setIsMobile(true);
+    else setIsMobile(false);
+  }, [width]);
+  if (isMobile) return <HeaderMobile />;
+
   return (
     <Grid templateColumns="repeat(3, 1fr)" as="header" py="3" px="8" boxShadow="md" bg="white" zIndex={100}>
       <GridItem colSpan={1} flexDirection="column">
--- src/calendar.tsx
+++ src/calendar.tsx
@@ -1,10 +1,11 @@
 import { Box, Center, Flex, Heading, Text, VStack } from '@chakra-ui/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { TermContext } from './app/context/TermContext';
 import { Header, Content, SidebarContainer, Feedback } from './app/index';
 import { getCurrentTerm } from './app/shared/utils/terms';
 import { Term } from './fetchers';
+import useWindowDimensions from './shared/hooks/useWindowDimensions';
 
 export type SelectedCourse = {
   subject: string;
@@ -13,11 +14,60 @@ export type SelectedCourse = {
   title: string;
 };
 
+function CalendarMobile(): JSX.Element {
+  const [term, setTerm] = useState(getCurrentTerm());
+  const [, setQuery] = useState('');
+
+  const [selectedCourse] = useState<SelectedCourse>();
+
+  const handleSearchChange = (q: string) => {
+    setQuery(q);
+  };
+
+  return (
+    <TermContext.Provider value={{ term, setTerm }}>
+      <Flex h="100vh" direction="column">
+        <Header onSearchChange={handleSearchChange} />
+        <Box grow={1} overflow="hidden" height="100%">
+          <Flex color="white" height="100%">
+            <Flex minW="80%" overflow="auto" justifyContent="center" height="100%" boxShadow="lg" zIndex={56}>
+              {selectedCourse ? (
+                <Content term={term as Term} selectedCourse={selectedCourse} />
+              ) : (
+                <Center p="10">
+                  <VStack>
+                    <Heading color="black">Explore Courses</Heading>
+                    <Text color="gray">
+                      Select a subject and then a course to start viewing course details and section information.
+                    </Text>
+                  </VStack>
+                </Center>
+              )}
+            </Flex>
+          </Flex>
+          <Box pos="absolute" bottom="0" right="0" zIndex={999} p={25}>
+            <Feedback />
+          </Box>
+        </Box>
+      </Flex>
+    </TermContext.Provider>
+  );
+}
+
 export function Calendar(): JSX.Element | null {
+  const [isMobile, setIsMobile] = useState(false);
   const [term, setTerm] = useState(getCurrentTerm());
   const [query, setQuery] = useState('');
 
   const [selectedCourse, setSelectedCourse] = useState<SelectedCourse>();
+
+  const { width } = useWindowDimensions();
+
+  useEffect(() => {
+    if (width < 480) setIsMobile(true);
+    else setIsMobile(false);
+  }, [width]);
+  if (isMobile) return <CalendarMobile />;
 
   const handleSearchChange = (q: string) => {
     setQuery(q);
