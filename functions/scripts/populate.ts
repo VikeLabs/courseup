6471a0582270f31f1cda2f0eed3a55b2472d1827
Aomi <aomijokoji@gmail.com>
2021-04-14 21:36:51 -0700

    feat: local db data population script

---
+++ functions/scripts/populate.ts
@@ -0,0 +1,17 @@
+import * as admin from 'firebase-admin';
+admin.initializeApp({
+  projectId: 'staging-clockwork',
+});
+
+import { CoursesService } from '../src/courses/Course.service';
+
+
+const term = '202105'
+
+const main = async () => {
+  console.log(`populating Firestore with data for term = ${term}`)
+  await CoursesService.populateCourses(term);
+  console.log(`finished.`)
+};
+
+main();
