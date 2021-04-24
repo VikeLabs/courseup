57fbd71ab6b3ea4f794e72de8b23a137cc7360d7
Alex <zthh626@gmail.com>
2021-04-24 11:35:39 -0700

    Display event colours according to event title

--- src/app/scheduler/Scheduler.tsx
+++ src/app/scheduler/Scheduler.tsx
@@ -1,4 +1,6 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, Spinner } from '@chakra-ui/react';
+
+import { Course, useGetCourse, useGetCourses, useSections } from '../../fetchers';
 
 import { SchedulerCalendar } from './components/SchedulerCalendar';
 
@@ -8,8 +10,12 @@ export interface CalendarProps {}
  * Primary UI component for content
  */
 export function Scheduler(): JSX.Element {
+  const term = '202105';
+  const { data: courses } = useGetCourses({ term });
+
   return (
     <Box w="100%" height="100%">
+      {/* {courses != undefined && <SchedulerCalendar courses={courses} />} */}
       <SchedulerCalendar />
     </Box>
   );
--- src/app/scheduler/components/SchedulerCalendar.tsx
+++ src/app/scheduler/components/SchedulerCalendar.tsx
@@ -1,16 +1,21 @@
 import { Box } from '@chakra-ui/layout';
 import moment from 'moment';
-import React from 'react';
 import 'react-big-calendar/lib/sass/styles.scss';
-import { Calendar, momentLocalizer, EventProps, Event } from 'react-big-calendar';
+import { Calendar, momentLocalizer, Event } from 'react-big-calendar';
 
-export interface CalendarProps {}
+import { ClassScheduleListing, Course, Section, useSections } from '../../../fetchers';
 
 const localizer = momentLocalizer(moment);
 
-const eventStyleGetter = () => {
+const titles: Map<string, string> = new Map();
+
+titles.set('event1', 'green');
+titles.set('event2', 'red');
+titles.set('event3', 'Blue');
+
+const eventStyleGetter = ({ title }: Event) => {
   var style = {
-    backgroundColor: 'green',
+    backgroundColor: title ? titles.get(title) : 'blue',
     borderRadius: '0px',
     opacity: 0.8,
     color: 'black',
@@ -22,37 +27,42 @@ const eventStyleGetter = () => {
   };
 };
 
-class EventComponent extends React.Component<EventProps<Event>> {
-  render() {
-    return (
-      <Box background="green">
-        <h1
-          onClick={() => {
-            console.log('hello world');
-          }}
-        >
-          :))))) it works
-        </h1>
-      </Box>
-    );
-  }
-}
-
 const events: Event[] = [
   {
-    title: 'All Day Event very long title',
+    title: 'event1',
     allDay: false,
-    start: new Date(2015, 3, 0),
-    end: new Date(2015, 3, 1),
+    start: new Date(2015, 3, 0, 5, 0, 0),
+    end: new Date(2015, 3, 0, 6, 0, 0),
   },
   {
-    title: 'Some Event',
+    title: 'event2',
+    allDay: false,
     start: new Date(2015, 3, 9, 1, 0, 0),
     end: new Date(2015, 3, 9, 2, 0, 0),
   },
+  {
+    title: 'event3',
+    allDay: false,
+    start: new Date(2015, 3, 9, 3, 0, 0),
+    end: new Date(2015, 3, 9, 4, 0, 0),
+  },
 ];
 
-export function SchedulerCalendar({}: CalendarProps): JSX.Element {
+export interface CalendarProps {
+  courses?: Course[];
+}
+
+export function SchedulerCalendar({ courses }: CalendarProps): JSX.Element {
+  const term = '202105';
+  const classScheduleListing: ClassScheduleListing[] | null = new Array();
+
+  // for (var i = 0; i < courses.length; i++) {
+  //   const subject = courses[i].subject;
+  //   const code = courses[i].code;
+  //   const { data: scehdulelisting } = useSections({ term, queryParams: { subject, code } });
+  //   classScheduleListing.push(scehdulelisting);
+  // }
+
   return (
     <Box h="100%" w="100%" p="2em">
       <Calendar
@@ -61,9 +71,7 @@ export function SchedulerCalendar({}: CalendarProps): JSX.Element {
         view="work_week"
         views={['work_week']}
         defaultDate={new Date(2015, 3, 1)}
-        components={{
-          event: EventComponent,
-        }}
+        eventPropGetter={eventStyleGetter}
       />
     </Box>
   );
