fecf4e396479bbf345cdf31acd91b3ee53f62a3c
Keith Radford <53020925+keithradford@users.noreply.github.com>
2021-04-27 21:13:12 -0700

    Add links to UVic pages at bottom of content (#107)
    * add source to uvic pages at bottom of content
    
    * make links static
    
    * sources always at bottom
    
    * add section source
    
    * section source => uvic
    
    * styley things
    
    * remove year
    
    * add margin-bottom
    
    * smaller margin cuz i can't make up my mind
    
    * nothing to see here o_o

--- src/app/content/Content.tsx
+++ src/app/content/Content.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Skeleton } from '@chakra-ui/react';
+import { Box, Center, Flex, Heading, Skeleton, Spacer, Text } from '@chakra-ui/react';
 import { useSearchParams } from 'react-router-dom';
 
 import { Term, useGetCourse } from '../../shared/fetchers';
@@ -6,13 +6,13 @@ import { Term, useGetCourse } from '../../shared/fetchers';
 import { CourseInfo } from './components/Course';
 import { SectionsContainer } from './containers/Section';
 
-export interface ContentProps {
+export type ContentProps = {
   /**
    * Term Selected
    * Determines what term the subjects and courses are from
    */
   term: Term;
-}
+};
 
 /**
  * Primary UI component for content
@@ -22,36 +22,64 @@ export function Content({ term }: ContentProps): JSX.Element {
   const { data, loading } = useGetCourse({ term, pid: searchParams.get('pid') || '' });
 
   return (
-    <Box width={['container.md', 'container.lg', 'container.xl']} bg="white" p={4} zIndex={60}>
-      <Flex
-        justifyItems="center"
-        alignItems={{ base: 'start', sm: 'center' }}
-        direction={{ base: 'column', sm: 'row' }}
-      >
-        <Heading mr="5" size="2xl" as="h2" whiteSpace="pre" color="black">{`${data?.subject || ''} ${
-          data?.code || ''
-        }`}</Heading>
-        {!loading && data && (
-          <Heading size="lg" as="h3" color="gray">
-            {data.title}
-          </Heading>
-        )}
-      </Flex>
-      <Skeleton isLoaded={!loading}>
-        {data && (
-          <>
-            <CourseInfo
-              subject={data.subject}
-              code={data.code}
-              title={data.title}
-              description={data.description || ''}
-              credits={data.credits}
-              hours={data.hoursCatalog}
-            />
-            <SectionsContainer term={term} subject={data?.subject} code={data?.code} />
-          </>
-        )}
-      </Skeleton>
-    </Box>
+    <Flex width={['container.md', 'container.lg', 'container.xl']} flexDirection="column" minH="100%">
+      <Box bg="white" p={4} zIndex={60}>
+        <Flex
+          justifyItems="center"
+          alignItems={{ base: 'start', sm: 'center' }}
+          direction={{ base: 'column', sm: 'row' }}
+        >
+          <Heading mr="5" size="2xl" as="h2" whiteSpace="pre" color="black">{`${data?.subject || ''} ${
+            data?.code || ''
+          }`}</Heading>
+          {!loading && data && (
+            <Heading size="lg" as="h3" color="gray">
+              {data.title}
+            </Heading>
+          )}
+        </Flex>
+        <Skeleton isLoaded={!loading}>
+          {data && (
+            <>
+              <CourseInfo
+                subject={data.subject}
+                code={data.code}
+                title={data.title}
+                description={data.description || ''}
+                credits={data.credits}
+                hours={data.hoursCatalog}
+              />
+              <SectionsContainer term={term} subject={data?.subject} code={data?.code} />
+            </>
+          )}
+        </Skeleton>
+      </Box>
+      <Spacer />
+      <Center>
+        <Text as="span" fontWeight="bold" fontSize={12} mb={2}>
+          Sources:
+          <Text as="span" color="blue.500" fontWeight="light">
+            <Text
+              as="a"
+              href={`https://www.uvic.ca/calendar/undergrad/index.php#/courses/${data?.pid}`}
+              target="_blank"
+              mx="3"
+              _hover={{ color: 'blue' }}
+            >
+              UVic Undergraduate Calendar
+            </Text>
+            <Text
+              as="a"
+              href={`https://www.uvic.ca/BAN1P/bwckctlg.p_disp_listcrse?term_in=${term}&subj_in=${data?.subject}&crse_in=${data?.code}&schd_in=`}
+              target="_blank"
+              _hover={{ color: 'blue' }}
+              ml="2"
+            >
+              UVic Class Schedule Listings
+            </Text>
+          </Text>
+        </Text>
+      </Center>
+    </Flex>
   );
 }
