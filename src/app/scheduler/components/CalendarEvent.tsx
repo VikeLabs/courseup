5a4929f6f3db1f39d10b1d2f8d96a6f78a0081c9
Alex Huang <31324580+zthh626@users.noreply.github.com>
2021-04-28 20:19:28 -0700

    Adding react-big-calendar for scheduler (#86)
    * Initial testing of react-big-calendar
    
    * Display event colours according to event title
    
    * Create events from CalendarEvents
    
    * Visual improvements
    
    * Add missing dependancies
    
    * Fix timezones + some removing box styling
    
    * Prevent crashing when trying to add events
    
    * Check for tba in meetingTime.time
    
    * Fix overflow
    
    * Fix a warning
    
    * Clean up for PR review
    
    * Lint
    
    * var -> const style, using indexOf for comparison
    
    * Use date-fns instead of moment.js
    
    * Replace require for import

---
+++ src/app/scheduler/components/CalendarEvent.tsx
@@ -0,0 +1,9 @@
+import { MeetingTimes } from '../../../shared/fetchers';
+
+export interface CalendarEvent {
+  subject: String;
+  code: String;
+  meetingTime: MeetingTimes;
+  sectionCode: String;
+  color: String;
+}
