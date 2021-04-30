e2da55cda705b1555a3d6e549d013b5d98e77ae8
Manav Shergill <shergillmanav@uvic.ca>
2021-04-30 13:40:51 -0700

    Deleted bookmark popup from header

--- src/app/header/Header.tsx
+++ src/app/header/Header.tsx
@@ -1,7 +1,6 @@
 import { Center, Grid, GridItem, Image, Button, Flex } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
-import { Bookmarks } from './components/Bookmarks';
 import { NavButtons } from './components/NavButtons';
 import { Search } from './components/SearchBar';
 import { TermButtons } from './components/TermButtons';
@@ -49,9 +48,6 @@ export function Header({ onSearchChange }: HeaderProps): JSX.Element {
       </GridItem>
       <GridItem colStart={3} alignContent="flex-end">
         <TermButtons />
-      </GridItem>
-      <GridItem>
-        <Bookmarks />
       </GridItem>
     </Grid>
   );
