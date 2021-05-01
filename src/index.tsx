96af23effab6cdf1f7ba18a2fbcbda78adbf1586
Keith <khr8771@gmail.com>
2021-05-01 09:30:36 -0700

    move mobile warning from routes to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import {
+  ChakraProvider,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+  useMediaQuery,
+} from '@chakra-ui/react';
 import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
 import algoliasearch from 'algoliasearch';
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { Helmet } from 'react-helmet';
 import { InstantSearch } from 'react-instantsearch-dom';
@@ -25,12 +34,37 @@ Sentry.init({
 
 const searchClient = algoliasearch('CR92D3S394', '5477854d63b676fe021f8f83f5839a3a');
 
+function Mobile(): JSX.Element | null {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isMobile] = useMediaQuery('(max-width: 600px)');
+
+  useEffect(() => {
+    onOpen();
+  }, [onOpen]);
+
+  if (!isMobile) return null;
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+      <ModalContent w="90%" minW="200px" zIndex={999}>
+        <ModalBody textAlign="center">
+          (╯°□°)╯︵ ┻━┻<Text my="5px"> Hey! We're still not quite there with mobile support.</Text>
+          <Text>
+            Please use a <Text as="strong">tablet</Text> or <Text as="strong">desktop</Text> to get the best experience
+          </Text>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <RestfulProvider base={process.env.NODE_ENV === 'production' ? '/api' : 'https://clockwork.vikelabs.dev/api'}>
       <InstantSearch searchClient={searchClient} indexName="dev_uvic">
         <ChakraProvider portalZIndex={999}>
           <Helmet titleTemplate="%s · clockwork" defaultTitle="clockwork · We make school easier" />
+          <Mobile />
           <Routes />
         </ChakraProvider>
       </InstantSearch>
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,44 +1,14 @@
-import { useDisclosure } from '@chakra-ui/hooks';
-import { Text } from '@chakra-ui/layout';
-import { useMediaQuery } from '@chakra-ui/media-query';
-import { Modal, ModalBody, ModalContent, ModalOverlay } from '@chakra-ui/modal';
-import { useEffect } from 'react';
 import { BrowserRouter, Routes as ReactRouterRoutes, Route } from 'react-router-dom';
 
 import { Calendar } from './pages/calendar';
 import { Home } from './pages/home';
 import { Scheduler } from './pages/scheduler/scheduler';
 
-// For our lack of mobile support :-)
-function Mobile(): JSX.Element {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  useEffect(() => {
-    onOpen();
-  }, [onOpen]);
-
-  return (
-    <Modal isOpen={isOpen} onClose={onClose} isCentered>
-      <ModalOverlay />
-      <ModalContent w="90%" minW="200px" zIndex={999}>
-        <ModalBody textAlign="center">
-          (╯°□°)╯︵ ┻━┻<Text my="5px"> Hey! We're still not quite there with mobile support.</Text>
-          <Text>
-            Please use a <Text as="strong">tablet</Text> or <Text as="strong">desktop</Text> to get the best experience
-          </Text>
-        </ModalBody>
-      </ModalContent>
-    </Modal>
-  );
-}
-
 // TODO: use nested routes but it doesn't work right now
-export function Routes(): JSX.Element {
-  const [isMobile] = useMediaQuery('(max-width: 600px)');
 
+export function Routes(): JSX.Element {
   return (
     <BrowserRouter>
-      {isMobile && <Mobile />}
       <ReactRouterRoutes>
         <Route path="/" element={<Home />} />
         <Route path="/calendar" element={<Calendar />} />
