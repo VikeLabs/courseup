63141cab2168deaf759abb6fd47500b6b3dafaeb
Keith <khr8771@gmail.com>
2021-05-01 11:41:00 -0700

    move to separate mobile component

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,14 @@
-import {
-  ChakraProvider,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalOverlay,
-  Text,
-  useDisclosure,
-  useMediaQuery,
-} from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
 import algoliasearch from 'algoliasearch';
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Helmet } from 'react-helmet';
 import { InstantSearch } from 'react-instantsearch-dom';
 import { RestfulProvider } from 'restful-react';
 
+import { Mobile } from './app/mobile';
 import reportWebVitals from './reportWebVitals';
 import { Routes } from './routes';
 
@@ -33,30 +25,6 @@ Sentry.init({
 });
 
 const searchClient = algoliasearch('CR92D3S394', '5477854d63b676fe021f8f83f5839a3a');
-
-function Mobile(): JSX.Element | null {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [isMobile] = useMediaQuery('(max-width: 600px)');
-
-  useEffect(() => {
-    onOpen();
-  }, [onOpen]);
-
-  if (!isMobile) return null;
-  return (
-    <Modal isOpen={isOpen} onClose={onClose} isCentered>
-      <ModalOverlay />
-      <ModalContent w="90%" minW="200px" zIndex={999}>
-        <ModalBody textAlign="center">
-          (╯°□°)╯︵ ┻━┻<Text my="5px"> Hey! We're still not quite there with mobile support.</Text>
-          <Text>
-            Please use a <Text as="strong">tablet</Text> or <Text as="strong">desktop</Text> to get the best experience
-          </Text>
-        </ModalBody>
-      </ModalContent>
-    </Modal>
-  );
-}
 
 ReactDOM.render(
   <React.StrictMode>
