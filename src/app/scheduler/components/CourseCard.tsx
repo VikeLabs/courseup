7cbd62a9e8f0c195daa6854cfcfe48c1af6575af
j-raymer <joeygatacre4@gmail.com>
2021-05-06 18:15:33 -0700

    minor changes

--- src/app/scheduler/components/CourseCard.tsx
+++ src/app/scheduler/components/CourseCard.tsx
@@ -1,72 +1,92 @@
-/* eslint-disable prettier/prettier */
 import { AddIcon, CloseIcon, InfoOutlineIcon } from '@chakra-ui/icons';
-import { Box, Text, Flex, VStack, Checkbox, IconButton } from '@chakra-ui/react';
+import { Box, Text, Flex, VStack, Checkbox, IconButton, Link } from '@chakra-ui/react';
 import { useCallback } from 'react';
 
-import { useGetCourse, Term } from '../../../shared/fetchers';
+import { Term } from '../../../shared/fetchers';
 import { useSavedCourses } from '../../../shared/hooks/useSavedCourses';
 
 export type CourseCardProps = {
-    term: Term;
-    subject: string;
-    title: string;
-    code: string;
-    colour: string;
-    pid: string;
-    selected: boolean;
-    handleChange: Function;
-    handleInfo: Function;
-}
+  term: Term;
+  subject: string;
+  title: string;
+  code: string;
+  colour: string;
+  pid: string;
+  selected: boolean;
+  handleChange: Function;
+};
 
-export function CourseCard({ term, subject, title, code, colour, pid, handleChange, handleInfo, selected }: CourseCardProps): JSX.Element {
-    const { data } = useGetCourse({ term, pid: pid || '' })
-    const { addCourse, deleteCourse, contains } = useSavedCourses()
-    const courseIsSaved = contains(data?.pid!, term);
+export function CourseCard({
+  term,
+  subject,
+  title,
+  code,
+  colour,
+  pid,
+  handleChange,
+  selected,
+}: CourseCardProps): JSX.Element {
+  const { addCourse, deleteCourse, contains } = useSavedCourses();
+  const courseIsSaved = contains(pid, term);
 
-    const onCourseClick = useCallback(() => {
-        if (data) {
-            if (!courseIsSaved) {
-                addCourse({ subject: data.subject, code: data.code, pid: data.pid, term });
-            } else {
-                deleteCourse({ subject: data.subject, code: data.code, pid: data.pid, term })
-            }
-        }
-    }, [data, courseIsSaved, addCourse, term, deleteCourse])
-
-    const onInfo = () => {
-        handleInfo()
+  const onCourseClick = useCallback(() => {
+    if (term && pid !== '' && code !== '') {
+      if (!courseIsSaved) {
+        addCourse({ subject, code, pid, term });
+      } else {
+        deleteCourse({ subject, code, pid, term });
+      }
     }
+  }, [term, pid, code, courseIsSaved, addCourse, subject, deleteCourse]);
 
-    const onChange = () => {
-        handleChange();
-    }
+  const onChange = useCallback(() => {
+    handleChange();
+  }, [handleChange]);
 
-    return (
-        <Box
-            boxShadow="md"
-            cursor="pointer"
-        >
-            <Flex direction="row" background={!selected ? 'blackAlpha.200' : 'white'}>
-                <Flex background={colour} alignItems="center" justifyContent="center" mr="10px">
-                    <Flex>
-                        <Checkbox backgroundColor="whiteAlpha.600" colorScheme="whiteAlpha" iconColor="black" size="lg" mx="7px" isChecked={selected} onChange={onChange} />
-                    </Flex>
-                </Flex>
-                <Flex direction="row" alignItems="center" justifyContent="space-between" w="100%" >
-                    <VStack alignItems="start" spacing="0" py="2">
-                        <Text fontSize="lg" fontWeight="bold">
-                            {subject} {code}
-                        </Text>
-                        <Text fontSize="sm" fontWeight="normal">
-                            {title}
-                        </Text>
-                    </VStack>
-                    <VStack alignContent="right" pr="5px" py="5px">
-                        <IconButton aria-label="Add to Scheduler" icon={!courseIsSaved ? <AddIcon color="white" /> : <CloseIcon color="white" />} background={!courseIsSaved ? "green.400" : "red"} size="sm" onClick={onCourseClick} />
-                        <IconButton aria-label="More information" icon={<InfoOutlineIcon color="white" />} size="sm" background="blue.400" onClick={onInfo} />
-                    </VStack>
-                </Flex>
-            </Flex>
-        </Box >
-    );
+  return (
+    <Box boxShadow="md" cursor="pointer">
+      <Flex direction="row" background={!selected ? 'blackAlpha.200' : 'white'}>
+        <Flex background={colour} alignItems="center" justifyContent="center" mr="10px">
+          <Flex>
+            <Checkbox
+              backgroundColor="whiteAlpha.600"
+              colorScheme="whiteAlpha"
+              iconColor="black"
+              size="lg"
+              mx="7px"
+              isChecked={selected}
+              onChange={onChange}
+            />
+          </Flex>
+        </Flex>
+        <Flex direction="row" alignItems="center" justifyContent="space-between" w="100%">
+          <VStack alignItems="start" spacing="0" py="2">
+            <Text fontSize="lg" fontWeight="bold">
+              {subject} {code}
+            </Text>
+            <Text fontSize="sm" fontWeight="normal">
+              {title}
+            </Text>
+          </VStack>
+          <VStack alignContent="right" pr="5px" py="5px">
+            <IconButton
+              aria-label="Add to Scheduler"
+              icon={!courseIsSaved ? <AddIcon color="white" /> : <CloseIcon color="white" />}
+              background={!courseIsSaved ? 'green.400' : 'red'}
+              size="sm"
+              onClick={onCourseClick}
+            />
+            <IconButton
+              aria-label="More information"
+              icon={<InfoOutlineIcon color="white" />}
+              size="sm"
+              background="blue.400"
+              as={Link}
+              to={`/calendar/${term}/${subject}?pid=${pid}`}
+            />
+          </VStack>
+        </Flex>
+      </Flex>
+    </Box>
+  );
 }
