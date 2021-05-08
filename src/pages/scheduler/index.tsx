68087587c134a185a712c698dd9dd8c914b43686
Alex <zthh626@gmail.com>
2021-05-08 11:33:21 -0700

    Revert "temp data"
    This reverts commit 1632d6808f1da2e7d3bbcf016d62f50ac0103319.

--- src/pages/scheduler/index.tsx
+++ src/pages/scheduler/index.tsx
@@ -2,34 +2,16 @@ import { Flex } from '@chakra-ui/layout';
 import { useParams } from 'react-router';
 
 import { SchedulerContainer, SchedulerSidebar } from '../../app';
-import { Term, useSections } from '../../shared/fetchers';
+import { Term } from '../../shared/fetchers';
 import { SidebarTemplate } from '../../shared/SidebarTemplate';
 
 export function Scheduler(): JSX.Element {
-  const term = '202105';
-  const subject = 'ECE';
-  const code = '310';
-  const { data: scehdulelisting } = useSections({ term, queryParams: { subject, code } });
+  const { term } = useParams();
 
   return (
     <SidebarTemplate title="Scheduler" term={term as Term}>
       <Flex flexGrow={1}>
-        {scehdulelisting && (
-          <SchedulerContainer
-            calendarEvents={(() => {
-              const x = new Array();
-              x.push({
-                subject: subject,
-                code: code,
-                color: '#F58400',
-                textColor: 'white',
-                sectionCode: scehdulelisting[0].sectionCode,
-                meetingTime: scehdulelisting[0].meetingTimes[0],
-              });
-              return x;
-            })()}
-          />
-        )}
+        <SchedulerContainer />
         <SchedulerSidebar />
       </Flex>
     </SidebarTemplate>
