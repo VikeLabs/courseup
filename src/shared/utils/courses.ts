62bbb8a5da1b7b448eaa7f00aeb2ae6e4f87b361
Aomi <aomijokoji@gmail.com>
2021-05-19 18:01:16 -0700

    refactor: simplify and add missing section types

--- src/shared/utils/courses.ts
+++ src/shared/utils/courses.ts
@@ -1,25 +1,9 @@
-import { Section } from '../fetchers';
+import { Section, SectionType } from '../fetchers';
 
-type sectionType = 'lecture' | 'lab' | 'tutorial';
-
-export const getFirstSectionType = (sections: Section[], type: sectionType): number => {
-  let i = 0;
-  for (const section of sections) {
-    if (section.sectionType === type) {
-      return i;
-    }
-    i++;
-  }
-
-  return -1;
+export const getFirstSectionType = (sections: Section[], type: SectionType): number => {
+  return sections.findIndex((section) => section.sectionType === type);
 };
 
-export const hasSectionType = (sections: Section[], type: sectionType): boolean => {
-  for (const section of sections) {
-    if (section.sectionType === type) {
-      return true;
-    }
-  }
-
-  return false;
+export const hasSectionType = (sections: Section[], type: SectionType): boolean => {
+  return sections.some((section) => section.sectionType === type);
 };
