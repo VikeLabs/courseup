bea0339802aa2c72de601b93067af5a7b9912e2e
Aomi <aomijokoji@gmail.com>
2021-05-21 18:11:04 -0700

    fix: type response

--- src/shared/api/getSections.ts
+++ src/shared/api/getSections.ts
@@ -1,10 +1,12 @@
+import { Section } from '../fetchers';
+
 interface getSectionsProps {
   term: string;
   subject: string;
   code: string;
 }
 
-export const getSections = async ({ term, subject, code }: getSectionsProps) => {
+export const getSections = async ({ term, subject, code }: getSectionsProps): Promise<Section[]> => {
   const response = await fetch(`/api/sections/${term}?subject=${subject}&code=${code}`);
   return response.json();
 };
