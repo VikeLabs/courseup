7464264f70aad80add64b83e1426f8e5fb713130
Aomi <aomijokoji@gmail.com>
2021-05-30 17:39:15 -0700

    chore: format

--- functions/scripts/populate-courses.ts
+++ functions/scripts/populate-courses.ts
@@ -4,11 +4,12 @@ import { Term } from '../src/constants';
 
 admin.initializeApp({ credential: admin.credential.applicationDefault() });
 
-if(process.argv.length != 3) throw Error('Term argument not found.')
+if (process.argv.length != 3) throw Error('Term argument not found.');
 
 const term = process.argv[2];
 
-if(!/20\d{2}0[1,5,9]/.test(term.trim())) throw Error('Invalid term argument format')
+if (!/20\d{2}0[1,5,9]/.test(term.trim()))
+  throw Error('Invalid term argument format');
 
 const main = async () => {
   console.log('populating database with courses');
