8196e3fb45f4f2a80fd88de21894a21ba1c57894
Manav Shergill <shergillmanav@uvic.ca>
2021-05-31 21:45:29 -0700

    removed unused things

--- src/app/header/components/Bookmarks.tsx
+++ src/app/header/components/Bookmarks.tsx
@@ -1,25 +1,16 @@
-import { CloseIcon } from '@chakra-ui/icons';
 import {
   Popover,
   PopoverTrigger,
   PopoverContent,
   PopoverBody,
   Portal,
-  Text,
   Button,
-  Spacer,
   Box,
-  IconButton,
   ButtonGroup,
-  Flex,
 } from '@chakra-ui/react';
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 
-import { MeetingTimes, useGetCourse, Term } from '../../../shared/fetchers';
 import { Course, useSavedCourses } from '../../../shared/hooks/useSavedCourses';
-import { SidebarTemplate } from '../../../shared/SidebarTemplate';
-import { CourseCard } from '../../scheduler/components/CourseCard';
-import { getCurrentTerm } from '../../shared/utils/terms';
 import { Card } from '../../sidebar/components/Card';
 
 export function Bookmarks(): JSX.Element {
@@ -41,9 +32,6 @@ export function Bookmarks(): JSX.Element {
         <PopoverContent bgColor="#E4E4E4" overflowY="auto" minW="300px" boxShadow="md" h="80vh" borderColor="blue.800">
           <PopoverBody>
             {courses.map((course: Course) => {
-              const term = course.term as Term;
-              const pid = course.pid;
-              // const { data, loading } = useGetCourse({ term: term, pid });
               return (
                 <Box w="100%" padding="3px">
                   <Card subject={course.subject} code={course.code} pid={course.pid} title="" schedule />
