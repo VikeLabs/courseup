d592fca4829032cdaafdf3e5f8d944fe5400fc2e
Keith <khr8771@gmail.com>
2021-06-06 10:08:55 -0700

    add copy functionality for crns

--- src/app/registration/components/RegistrationSection.tsx
+++ src/app/registration/components/RegistrationSection.tsx
@@ -1,6 +1,10 @@
+import { IconButton } from '@chakra-ui/button';
 import { Checkbox } from '@chakra-ui/checkbox';
+import { useClipboard } from '@chakra-ui/hooks';
 import { Box, Heading, HStack, Text } from '@chakra-ui/layout';
-import { useCallback } from 'react';
+import { useToast } from '@chakra-ui/toast';
+import { useCallback, useEffect } from 'react';
+import { IoCopyOutline } from 'react-icons/io5';
 
 import { Seat } from '../../../shared/fetchers';
 import { SeatInfo } from '../../content/components/Seats';
@@ -14,6 +18,13 @@ type Props = {
 };
 
 export function RegistrationSection({ section, crn, seats, selected, handleChange }: Props) {
+  const { hasCopied, onCopy } = useClipboard(crn);
+  const toast = useToast();
+
+  useEffect(() => {
+    hasCopied && toast({ status: 'success', title: `Copied ${crn} to clipboard!` });
+  }, [crn, hasCopied, toast]);
+
   const onChange = useCallback(() => {
     handleChange({ crn, seats, selected });
   }, [crn, handleChange, seats, selected]);
@@ -25,6 +36,19 @@ export function RegistrationSection({ section, crn, seats, selected, handleChang
           {section}{' '}
           <Text as="span" fontWeight="normal" ml="2" color="rgb(155, 155, 155)">
             {crn}
+            <IconButton
+              icon={<IoCopyOutline />}
+              onClick={onCopy}
+              aria-label="copy"
+              colorScheme="white"
+              color="rgb(155, 155, 155)"
+              _focus={{
+                outline: 'none',
+              }}
+              _active={{
+                color: 'black',
+              }}
+            />
           </Text>
         </Heading>
         <Checkbox isChecked={selected} onChange={onChange}>
