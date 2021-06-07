8e4979fcbeee82cd6ee9b2e8060272828aefc691
Keith <khr8771@gmail.com>
2021-06-06 17:41:21 -0700

    small refactors

--- src/app/registration/components/RegistrationSection.tsx
+++ src/app/registration/components/RegistrationSection.tsx
@@ -37,11 +37,11 @@ export function RegistrationSection({ section, crn, seats, additionalNotes, sele
         <Heading size="md" as="h3" mt="2">
           {section}
         </Heading>
-        <HStack spacing="3">
-          <Checkbox isChecked={selected} onChange={onChange}>
+        <HStack spacing="3" top="50%" minW="205px">
+          <Checkbox isChecked={selected} onChange={onChange} h="32px">
             Registered
           </Checkbox>
-          <Heading size="md" as="h3" color="gray">
+          <Heading size="md" as="h3" color="gray" h="32px">
             {crn}
             <IconButton
               icon={<IoCopyOutline />}
@@ -54,6 +54,7 @@ export function RegistrationSection({ section, crn, seats, additionalNotes, sele
               _focus={{
                 outline: 'none',
                 color: 'rgb(19, 135, 243)',
+                fontSize: '22.5px',
               }}
             />
           </Heading>
