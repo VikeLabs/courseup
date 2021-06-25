611a5d37f3424b9997f36cf1de2228d7fca0bef0
Keith <khr8771@gmail.com>
2021-06-24 17:17:53 -0700

    feat: add tips in banner

--- src/common/header/containers/HeaderContainer.tsx
+++ src/common/header/containers/HeaderContainer.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState, useCallback } from 'react';
+
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import {
   Center,
   Grid,
@@ -9,8 +12,8 @@ import {
   Collapse,
   Alert,
   CloseButton,
-  Link,
   AlertDescription,
+  IconButton,
 } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
 
@@ -24,26 +27,73 @@ export interface HeaderProps {
   onSearchChange?: (query: string) => void;
   onSearchSubmit?: (query: string) => void;
 }
+
 /**
  * Primary UI component for content
  */
 export function HeaderContainer({ onSearchChange }: HeaderProps): JSX.Element {
   const [banner, setBanner] = useSessionStorage('user:banner', true);
+  const [tipIndex, setTipIndex] = useState(0);
+
+  const tips = [
+    '💡 Your courses and sections are saved between sessions, no need to leave the tab open!',
+    "💡 Press the 'Register' button while viewing your timetable to help you quickly register for classes!",
+    "💡 See something you don't like or think might be a bug? Send feedback to the team via the button at the bottom right!",
+    '💡 Courses that appear transparent on your timetable mean that section happens during that time, but not during the week you are viewing.',
+    "⚠️ We're in beta right now so expect things to be a bit rocky. ⚠️",
+  ];
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setTipIndex((tipIndex + 1) % tips.length);
+    }, 2 * 60 * 100);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [tipIndex, tips.length]);
+
+  const back = useCallback(() => {
+    tipIndex - 1 < 0 ? setTipIndex(tips.length - 1) : setTipIndex(tipIndex - 1);
+  }, [tipIndex, tips.length]);
+
+  const forward = useCallback(() => {
+    setTipIndex((tipIndex + 1) % tips.length);
+  }, [tipIndex, tips.length]);
+
   return (
     <Box zIndex={1000}>
       <Collapse in={banner} animateOpacity>
         <Alert status="success" alignItems="center" justifyContent="center" variant="solid">
-          <AlertDescription>
-            ☕ Hey! We're holding <b>office hours</b> and a <b>backend developer workshop</b> for CourseUp on June, 19th
-            at 11 am PDT on the{' '}
-            <Link href="https://discord.gg/ZhpnafrxKQ" fontWeight="bold" textDecoration="underline" isExternal>
-              VikeLabs Discord
-            </Link>
-            {'! '}
-            <Link href="https://github.com/VikeLabs/courseup/discussions/218" textDecoration="underline" isExternal>
-              Click here to learn more!
-            </Link>
-          </AlertDescription>
+          <IconButton
+            aria-label="idk yet ill change this"
+            icon={<ChevronLeftIcon />}
+            bgColor="transparent"
+            _hover={{
+              bgColor: 'transparent',
+              color: '#707070',
+            }}
+            size="lg"
+            h="fit-content"
+            mx={2}
+            onClick={back}
+          />
+          <Center w="1100px">
+            <AlertDescription>{`${tips[tipIndex]}`}</AlertDescription>
+          </Center>
+          <IconButton
+            aria-label="idk yet ill change this"
+            icon={<ChevronRightIcon />}
+            bgColor="transparent"
+            size="lg"
+            h="fit-content"
+            _hover={{
+              bgColor: 'transparent',
+              color: '#707070',
+            }}
+            mx={2}
+            onClick={forward}
+          />
           <CloseButton position="absolute" right="8px" top="8px" onClick={() => setBanner(false)} />
         </Alert>
       </Collapse>
@@ -56,6 +106,7 @@ export function HeaderContainer({ onSearchChange }: HeaderProps): JSX.Element {
         bg="#82cbee"
         zIndex={100}
         maxH="56px"
+        onClick={() => setTipIndex((tipIndex + 1) % tips.length)}
       >
         <GridItem colSpan={1}>
           <Flex justifyContent="space-between" alignContent="center" alignItems="flex-start">
