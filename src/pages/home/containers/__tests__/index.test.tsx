6f3a3f6e0a1fc8184fbe1bbe37a5f3d72ca27673
Aomi <aomijokoji@gmail.com>
2021-07-03 12:16:59 -0700

    fix: import

--- src/pages/home/containers/__tests__/index.test.tsx
+++ src/pages/home/containers/__tests__/index.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import { axe } from 'jest-axe';
 
-import Landing from '..';
+import { Landing } from 'pages/home/containers/Landing';
 
 describe('Landing', () => {
   it('has no a11y violations', async () => {
