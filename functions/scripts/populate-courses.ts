b626d7f154ea3ad5c22b6298345b6a388310c0bf
Aomi Jokoji <ajokoji@change.org>
2021-07-05 17:08:03 -0700

    feat: support local db population

--- functions/scripts/populate-courses.ts
+++ functions/scripts/populate-courses.ts
@@ -2,7 +2,12 @@ import { CoursesService } from '../src/courses/Course.service';
 import * as admin from 'firebase-admin';
 import { Term } from '../src/constants';
 
-admin.initializeApp({ credential: admin.credential.applicationDefault() });
+// to allow this script to run locally without fudging things around
+if (process.env.FIRESTORE_EMULATOR_HOST) {
+  admin.initializeApp({
+    projectId: 'development',
+  });
+}
 
 if (process.argv.length != 3) throw Error('Term argument not found.');
 
@@ -13,7 +18,7 @@ if (!/20\d{2}0[1,5,9]/.test(term.trim()))
 
 const main = async () => {
   console.log('populating database with courses');
-  await CoursesService.populateCourses(process.argv[2] as Term);
+  await CoursesService.populateCourses(term as Term);
 };
 
 main();
