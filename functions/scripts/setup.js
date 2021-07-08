ec3f5bfe1d3127d471cc43914aedb3479ee31281
Aomi Jokoji <ajokoji@change.org>
2021-07-07 19:10:43 -0700

    feat: postinstall script

---
+++ functions/scripts/setup.js
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { exec } = require('child_process');
+const fs = require('fs');
+
+// term to populate date for
+const term = '202101';
+
+// command to run to populate data
+const command = `npm run db:populate:local ${term}`;
+
+// directory to store firestore data
+const dir = 'tmp/firestore';
+
+if (fs.existsSync(dir) || process.env.CI) {
+  return 0;
+}
+
+fs.mkdirSync(dir, { recursive: true });
+
+exec(
+  `firebase emulators:exec "${command}" --import=${dir} --export-on-exit --project development --only firestore`,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.error(`error: ${error.message}`);
+      return;
+    }
+
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return;
+    }
+
+    console.log(`stdout:\n${stdout}`);
+  }
+);
