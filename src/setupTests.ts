b25a7a6e1b7ac36f5ef0e29ca58995619c00f4d8
Keith <khr8771@gmail.com>
2021-07-08 17:53:40 -0700

    nothing to see here

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,6 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
 import { toHaveNoViolations } from 'jest-axe';
 
 expect.extend(toHaveNoViolations);
