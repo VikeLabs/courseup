7eda0d8a456e9532697cc91c39bbab55985463fa
Eduardo Szeckir <eszeckirjr@gmail.com>
2021-08-04 19:53:45 -0700

    Changing button size to small so that it doesn't increase the header height

--- src/common/header/components/GitHubButton.tsx
+++ src/common/header/components/GitHubButton.tsx
@@ -7,6 +7,7 @@ export function GitHubButton(): JSX.Element {
       as="a"
       href="https://github.com/VikeLabs/courseup"
       target="_blank"
+      size="sm"
       colorScheme="none"
       fontSize="35px"
       isRound
