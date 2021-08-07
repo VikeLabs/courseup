4793810538289ee06947d4f4a8eb89c3cb5409fa
Eduardo Szeckir <eszeckirjr@gmail.com>
2021-08-07 15:02:04 -0700

    Creating modal that opens when button is pressed

--- src/pages/scheduler/components/ShareTimetableButton.tsx
+++ src/pages/scheduler/components/ShareTimetableButton.tsx
@@ -1,10 +1,37 @@
-import { Button, Icon } from '@chakra-ui/react';
+import {
+  Button,
+  Icon,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { IoShareOutline } from 'react-icons/io5';
 
 export default function ShareTimetableButton() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <Button size="sm" bg="blue.400" color="white" leftIcon={<Icon as={IoShareOutline} />}>
-      Share
-    </Button>
+    <>
+      <Button size="sm" bg="blue.400" color="white" leftIcon={<Icon as={IoShareOutline} />} onClick={onOpen}>
+        Share
+      </Button>
+
+      <Modal onClose={onClose} isOpen={isOpen} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody> Modal Content </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>Close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
