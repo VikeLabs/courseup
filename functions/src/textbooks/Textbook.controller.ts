314454ecd3a3f5f2efca3c59af0339448edc9520
Aomi <aomijokoji@gmail.com>
2021-08-22 21:55:08 -0700

    feat: textbooks endpoint

---
+++ functions/src/textbooks/Textbook.controller.ts
@@ -0,0 +1,35 @@
+import { CourseTextbooks } from '@vikelabs/uvic-course-scraper/dist';
+import { Get, Controller, Route, Response, Path } from 'tsoa';
+import { get } from 'typesaurus';
+import { Term } from '../constants';
+import { TextbooksCollection } from '../db/collections';
+
+/**
+ * @tsoaModel
+ */
+interface TextbookInfo {
+  subject: string;
+  code: string;
+  term: string;
+  sections: Omit<CourseTextbooks, 'subject' | 'code'>[];
+}
+
+@Route('textbooks')
+export class TextbooksController extends Controller {
+  @Response(404, 'Textbooks Not Found')
+  @Get('{term}/{subject}/{code}')
+  public async seats(
+    @Path() term: Term,
+    @Path() subject: string,
+    @Path() code: string
+  ): Promise<TextbookInfo | void> {
+    const books = await get(TextbooksCollection, `${term}${subject}${code}`);
+    if (!books) {
+      this.setStatus(404);
+      return;
+    }
+
+    this.setHeader('Cache-Control', 'public, max-age=1800, s-maxage=900');
+    return books.data;
+  }
+}
