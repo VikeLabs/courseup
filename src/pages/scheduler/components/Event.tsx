3575e54876819fb4232d9bebf7bb73cc2971d40d
Aomi <aomijokoji@gmail.com>
2021-09-01 15:52:36 -0700

    feat: colour mode aware calendar event component

--- src/pages/scheduler/components/Event.tsx
+++ src/pages/scheduler/components/Event.tsx
@@ -1,14 +1,23 @@
 import { Flex, Heading, HStack, VStack } from '@chakra-ui/react';
 import { EventProps as BaseEventProps } from 'react-big-calendar';
 
+import { useDarkMode } from 'lib/hooks/useDarkMode';
+
 import { CourseCalendarEvent } from 'pages/scheduler/shared/types';
 
 export type EventProps = BaseEventProps<{ resource: Omit<CourseCalendarEvent, 'meetingTime' | 'term'> }>;
 
 export const CalendarEvent = ({ title, event: { resource } }: EventProps) => {
+  const mode = useDarkMode();
   return (
     <Flex height="100%" direction="column">
-      <HStack w="100%" bg="#EDF2F7" justifyContent="space-between" p="0.2em">
+      <HStack
+        w="100%"
+        bg={mode('gray.100', 'dark.main')}
+        justifyContent="space-between"
+        p="0.2em"
+        color={mode('dark.main', 'white')}
+      >
         <Heading size="xs">{title}</Heading>
         <Heading size="xs">{resource && resource.sectionCode}</Heading>
       </HStack>
