f4811ff71f10773b8b64855adf9069f19833d76a
Eduardo Szeckir <eszeckirjr@gmail.com>
2021-10-19 08:18:29 -0700

    Fixing linting and importing warnings

--- src/pages/scheduler/components/Toolbar.tsx
+++ src/pages/scheduler/components/Toolbar.tsx
@@ -2,39 +2,41 @@ import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import { Button, Flex, Heading, HStack, IconButton, Text } from '@chakra-ui/react';
 import { ToolbarProps } from 'react-big-calendar';
 
-export const CalendarToolBar = (onSelectedDateChange: (date: Date) => void) => ({ label, date }: ToolbarProps) => {
-  const handleClick = (offset?: number) => () => {
-    if (offset) {
-      const d = new Date(date);
-      d.setDate(d.getDate() + offset);
-      onSelectedDateChange(d);
-    } else {
-      onSelectedDateChange(new Date());
-    }
+export const CalendarToolBar =
+  (onSelectedDateChange: (date: Date) => void) =>
+  ({ label, date }: ToolbarProps) => {
+    const handleClick = (offset?: number) => () => {
+      if (offset) {
+        const d = new Date(date);
+        d.setDate(d.getDate() + offset);
+        onSelectedDateChange(d);
+      } else {
+        onSelectedDateChange(new Date());
+      }
+    };
+    return (
+      <Flex pb="0.5em" justifyContent="space-between" alignItems="center">
+        <Heading size="md">Scheduler</Heading>
+        <Text fontSize="xl">{label}</Text>
+        <HStack pb="0.2em">
+          <Button size="sm" colorScheme="gray" onClick={handleClick()}>
+            Today
+          </Button>
+          <IconButton
+            aria-label="Previous Week"
+            colorScheme="gray"
+            icon={<ChevronLeftIcon />}
+            size="sm"
+            onClick={handleClick(-7)}
+          />
+          <IconButton
+            aria-label="Next Week"
+            colorScheme="gray"
+            icon={<ChevronRightIcon />}
+            size="sm"
+            onClick={handleClick(7)}
+          />
+        </HStack>
+      </Flex>
+    );
   };
-  return (
-    <Flex pb="0.5em" justifyContent="space-between" alignItems="center">
-      <Heading size="md">Scheduler</Heading>
-      <Text fontSize="xl">{label}</Text>
-      <HStack pb="0.2em">
-        <Button size="sm" colorScheme="gray" onClick={handleClick()}>
-          Today
-        </Button>
-        <IconButton
-          aria-label="Previous Week"
-          colorScheme="gray"
-          icon={<ChevronLeftIcon />}
-          size="sm"
-          onClick={handleClick(-7)}
-        />
-        <IconButton
-          aria-label="Next Week"
-          colorScheme="gray"
-          icon={<ChevronRightIcon />}
-          size="sm"
-          onClick={handleClick(7)}
-        />
-      </HStack>
-    </Flex>
-  );
-};
