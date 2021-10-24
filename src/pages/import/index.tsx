ea58870baac2ae363a69775c48591bf58785066d
Eduardo Szeckir <eszeckirjr@gmail.com>
2021-10-23 17:20:26 -0700

    Implementing backend

--- src/pages/import/index.tsx
+++ src/pages/import/index.tsx
@@ -1,45 +1,34 @@
-import { useMemo } from 'react';
-
 // import { useParams } from 'react-router';
-import { Heading, VStack, Box } from '@chakra-ui/layout';
+import { Heading, VStack, Flex, Box } from '@chakra-ui/layout';
+import { Spinner } from '@chakra-ui/react';
+import { useParams } from 'react-router';
 
-import { useSavedCourses } from 'lib/hooks/useSavedCourses';
+import { Timetable, useGetTimetable } from 'lib/fetchers';
+import { getReadableTerm } from 'lib/utils';
 
 import { Header } from 'common/header';
 
 import { ImportButtons } from './components/ImportButtons';
 import { ImportCalendar } from './components/ImportCalendar';
-import { useGetCourseSections } from 'pages/scheduler/hooks/useCalendarEvents';
-import { denormalizeCourseEvents } from 'pages/scheduler/hooks/useTransformedCalendarEvents';
 
 export function ImportTimetable(): JSX.Element {
-  // const { slug } = useParams();
+  const { slug } = useParams();
 
-  // const sample_slug = 'FPfGNnbdEiSI';
-
-  const term = '202109';
-  // the user's saved courses
-  const { courses } = useSavedCourses();
-  // extend the list of courses with section information
-  const coursesResult = useGetCourseSections(term, courses);
-  // transform, filter etc. the users selected courses.
-  const calendarEvents = useMemo(
-    () => denormalizeCourseEvents(coursesResult.status === 'loaded' ? coursesResult.data : []),
-    [coursesResult]
-  );
+  const { loading, data } = useGetTimetable({ slug: slug });
+  const parsedData = data as Timetable;
 
   return (
     <>
       <Header />
-      <VStack p={50} spacing={10}>
-        <Heading>Importing Timetable</Heading>
-        <ImportButtons />
-      </VStack>
-      <VStack>
-        <Box w="70%">
-          <ImportCalendar term={term} courseCalendarEvents={calendarEvents} />
-        </Box>
-      </VStack>
+      <Flex h="100vh" w="100vw">
+        <VStack w="100vw" p={50} spacing={10}>
+          <Heading>
+            {loading ? 'Viewing Timetable' : 'Viewing Timetable for ' + getReadableTerm(parsedData['term'])}
+          </Heading>
+          <ImportButtons loading={loading} />
+          <Box w="70vw">{loading ? <Spinner size="xl" /> : <ImportCalendar data={data as Timetable} />}</Box>
+        </VStack>
+      </Flex>
     </>
   );
 }
