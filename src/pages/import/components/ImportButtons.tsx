9c92134fa6e13789d8451770b5fb6735c35f58f4
Eduardo Szeckir <eszeckirjr@gmail.com>
2021-10-23 17:19:57 -0700

    Disabling buttons on load

--- src/pages/import/components/ImportButtons.tsx
+++ src/pages/import/components/ImportButtons.tsx
@@ -2,13 +2,13 @@ import { Button } from '@chakra-ui/button';
 import { HStack } from '@chakra-ui/layout';
 import { BsBoxArrowInDown, BsArrowRepeat } from 'react-icons/bs';
 
-export function ImportButtons(): JSX.Element {
+export function ImportButtons({ loading }: { loading: boolean }): JSX.Element {
   return (
     <HStack spacing={10}>
-      <Button rightIcon={<BsBoxArrowInDown />} colorScheme="purple">
+      <Button disabled={loading} rightIcon={<BsBoxArrowInDown />} colorScheme="purple">
         Import to Timetable
       </Button>
-      <Button rightIcon={<BsArrowRepeat />} colorScheme="green">
+      <Button disabled={loading} rightIcon={<BsArrowRepeat />} colorScheme="green">
         Replace Timetable
       </Button>
     </HStack>
