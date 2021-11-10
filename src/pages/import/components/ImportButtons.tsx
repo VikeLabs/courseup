2843af5778ac6adf347ff59ae201e1b5cf28db35
Eduardo Szeckir <eszeckir@uvic.ca>
2021-11-10 12:39:11 -0800

    Adding compare timetable button

--- src/pages/import/components/ImportButtons.tsx
+++ src/pages/import/components/ImportButtons.tsx
@@ -1,15 +1,21 @@
 import { HStack } from '@chakra-ui/layout';
+import { useMatch } from 'react-router';
 
 import { Timetable } from 'lib/fetchers';
 
+import { BackTimetable } from './BackTimetable';
+import { CompareTimetables } from './CompareTimetables';
 import { ImportTimetable } from './ImportTimetable';
 import { ReplaceTimetable } from './ReplaceTimetable';
 
 export function ImportButtons({ loading, data }: { loading: boolean; data: Timetable }): JSX.Element {
+  const compareMatch = useMatch('/c/*');
+
   return (
     <HStack spacing={10}>
       <ImportTimetable loading={loading} data={data} />
       <ReplaceTimetable loading={loading} data={data} />
+      {compareMatch ? <BackTimetable /> : <CompareTimetables loading={loading} data={data} />}
     </HStack>
   );
 }
