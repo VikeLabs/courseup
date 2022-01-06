596a4dcb79e1d6f7fd67a8b58beb5018380eb4c8
Keith <khr8771@gmail.com>
2022-01-05 16:51:36 -0800

    chore: remove unused hook

--- src/common/header/hooks/useChangeCallback.ts
+++
@@ -1,6 +0,0 @@
-import { useCallback } from 'react';
-
-export const useChangeCallback = (
-  onChange: (value: string) => void
-): ((event: React.ChangeEvent<HTMLInputElement>) => void) =>
-  useCallback((event: React.ChangeEvent<HTMLInputElement>) => onChange(event.target.value), [onChange]);
