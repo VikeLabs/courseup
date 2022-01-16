43d0facd7469e8131062ea2610a6aa0f4c50a7e3
Eduardo Szeckir <eszeckirjr@gmail.com>
2022-01-16 15:11:09 -0800

    Fixing test and adding more

--- src/pages/scheduler/components/share/__tests__/ShareLinkOptions.test.tsx
+++ src/pages/scheduler/components/share/__tests__/ShareLinkOptions.test.tsx
@@ -6,13 +6,26 @@ const slug: string = 'xGIvxVgsvCYa';
 describe('ShareLinkOptions', () => {
   describe('when loading', () => {
     it('should not render any display link', () => {
-      render(<ShareLinkOptions slug={slug} isSmallScreen={false} loading={true} />);
-      expect(screen.queryByText('/s/xGIvxVgsvCYa')).not.toBeInTheDocument();
+      const { container } = render(<ShareLinkOptions slug={slug} isSmallScreen={false} loading={true} />);
+      expect(container).not.toHaveTextContent('/s/xGIvxVgsvCYa');
     });
 
     it('should display `Loading...` message', () => {
       render(<ShareLinkOptions slug={slug} isSmallScreen={false} loading={true} />);
       expect(screen.queryByDisplayValue('Loading...')).toBeInTheDocument();
+    });
+  });
+
+  describe('when sending a slug', () => {
+    it('should correctly render the display link', () => {
+      render(<ShareLinkOptions slug={slug} isSmallScreen={false} loading={false} />);
+      expect(screen.getByDisplayValue(window.location.hostname + '/s/xGIvxVgsvCYa')).toBeInTheDocument();
+    });
+
+    it('should show five React social media buttons', () => {
+      const { container } = render(<ShareLinkOptions slug={slug} isSmallScreen={false} loading={false} />);
+      const buttons = container.getElementsByClassName('react-share__ShareButton');
+      expect(buttons).toHaveLength(5);
     });
   });
 });
