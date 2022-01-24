aa05ef846e700bad2e0fd26a2a2668fa02e9bf38
Eduardo Szeckir <eszeckirjr@gmail.com>
2022-01-23 21:00:30 -0800

    Adding function to get saved courses divded by term

--- src/lib/utils/courses.ts
+++ src/lib/utils/courses.ts
@@ -1,4 +1,5 @@
-import { Section, SectionType } from 'lib/fetchers';
+import { Section, SectionType, Term } from 'lib/fetchers';
+import { SavedCourse } from 'lib/hooks/useSavedCourses';
 
 export const getFirstSectionType = (sections: Section[], type: SectionType): number => {
   return sections.findIndex((section) => section.sectionType === type);
@@ -6,4 +7,20 @@ export const getFirstSectionType = (sections: Section[], type: SectionType): num
 
 export const hasSectionType = (sections: Section[], type: SectionType): boolean => {
   return sections.some((section) => section.sectionType === type);
+};
+
+/*
+  Given an array of type SavedCourse[], it returns
+  an object of format
+  {
+    Term: SavedCourse[]
+  }
+  with all courses split by the term they are saved in
+*/
+export const groupCoursesBy = (courses: SavedCourse[]) => {
+  return courses.reduce((prev: { [key: string]: SavedCourse[] }, curr: SavedCourse) => {
+    let arr: any[] = (prev[curr['term']] = prev[curr['term']] || []);
+    arr.push(curr);
+    return prev;
+  }, {} as Record<Term, SavedCourse[]>);
 };
