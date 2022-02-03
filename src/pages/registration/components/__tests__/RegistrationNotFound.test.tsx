10f196ba35e87d7419848946aebba0316026840a
liamshatzel <liam.shatzel@gmail.com>
2022-02-02 17:52:21 -0800

    Lint fixes for RegistrationNotFoundComponent tests

--- src/pages/registration/components/__tests__/RegistrationNotFound.test.tsx
+++ src/pages/registration/components/__tests__/RegistrationNotFound.test.tsx
@@ -1,20 +1,21 @@
 import { render, screen } from '@testing-library/react';
+
+import { MemoryRouter } from 'react-router';
+
 import { getCurrentTerm, getReadableTerm } from 'lib/utils/terms';
+import { renderWithRouter } from 'lib/utils/jest/index';
 
 import { axe } from 'jest-axe';
-import { renderWithRouter } from 'lib/utils/jest/index';
-import { MemoryRouter } from 'react-router';
 
 import { RegistrationNotFound } from '../RegistrationNotFound';
 
 const term = getCurrentTerm();
-
 it('is in the document', () => {
     renderWithRouter(<RegistrationNotFound />, { route: `/${getCurrentTerm()}` });
     const renderComponent = screen.getByTestId('registration-component-1');
     expect(renderComponent).toBeInTheDocument();
     expect(renderComponent).toHaveTextContent(`${getReadableTerm(term)}`);
-})
+});
 
 describe('RegistrationNotFound Component', () => {
     it('has no a11y violations', async () => {
