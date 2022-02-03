ee0863d155aec0916a547d8460015daf773e2c20
liamshatzel <liam.shatzel@gmail.com>
2022-02-02 17:48:29 -0800

    Fixed lint for RegistrationNotFoundComponent tests

--- src/pages/registration/components/__tests__/RegistrationNotFound.test.tsx
+++ src/pages/registration/components/__tests__/RegistrationNotFound.test.tsx
@@ -1,15 +1,11 @@
 import { render, screen } from '@testing-library/react';
-import ReactDOM from 'react-dom';
-import React from 'react'
-import { RegistrationNotFound } from '../RegistrationNotFound';
 import { getCurrentTerm, getReadableTerm } from 'lib/utils/terms';
 
 import { axe } from 'jest-axe';
 import { renderWithRouter } from 'lib/utils/jest/index';
-import { MemoryRouter, Router } from 'react-router';
-import { createMemoryHistory } from 'history';
-import { createBrowserHistory } from 'history';
+import { MemoryRouter } from 'react-router';
 
+import { RegistrationNotFound } from '../RegistrationNotFound';
 
 const term = getCurrentTerm();
 
@@ -18,7 +14,6 @@ it('is in the document', () => {
     const renderComponent = screen.getByTestId('registration-component-1');
     expect(renderComponent).toBeInTheDocument();
     expect(renderComponent).toHaveTextContent(`${getReadableTerm(term)}`);
-
 })
 
 describe('RegistrationNotFound Component', () => {
