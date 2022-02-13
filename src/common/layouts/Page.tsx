3ce9a6c9c21ca9cfa85648027048e663440f9685
Keith <khr8771@gmail.com>
2022-02-12 17:53:13 -0800

    readd boxShadow

--- src/common/layouts/Page.tsx
+++ src/common/layouts/Page.tsx
@@ -57,7 +57,7 @@ export function Page({ title, leftSidebar, rightSidebar, mobileSupport, children
           ) : (
             leftSidebar && !isMobile && <Sidebar>{leftSidebar}</Sidebar>
           )}
-          <Flex overflowY="auto" zIndex={56} w="100%" justifyContent="center" overflowX="hidden">
+          <Flex overflowY="auto" zIndex={56} w="100%" justifyContent="center" overflowX="hidden" boxShadow="md">
             {isMobile && query.length > 0 ? (
               <Sidebar>
                 <SearchResults />
