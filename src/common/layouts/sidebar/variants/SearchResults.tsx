84befb68e030c00193e464954edca80faa85744a
Keith <khr8771@gmail.com>
2022-02-15 18:48:00 -0800

    chore: resolve term outside of customhits

--- src/common/layouts/sidebar/variants/SearchResults.tsx
+++ src/common/layouts/sidebar/variants/SearchResults.tsx
@@ -1,14 +1,18 @@
 import { Flex } from '@chakra-ui/react';
+import { useParams } from 'react-router';
+
+import { getCurrentTerm } from 'lib/utils/terms';
 
 import { CustomHits } from '../components/SearchResults';
 import { TopBar } from '../components/TopBar';
 
 export function SearchResults() {
+  const { term } = useParams();
   return (
     <>
       <TopBar>Search Results</TopBar>
       <Flex direction="column" overflowY="auto">
-        <CustomHits />
+        <CustomHits term={term ?? getCurrentTerm()} />
       </Flex>
     </>
   );
