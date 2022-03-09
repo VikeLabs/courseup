0b9fd3fc75cab83e9cba63d862905e887f4da0dd
Aomi <aomijokoji@gmail.com>
2022-03-08 18:52:12 -0800

    fix: remove landing component for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import { Center } from '@chakra-ui/react';
 
-import { Landing } from '../src/pages/home/containers/Landing';
-
 function HomePage() {
   return (
     <Center h="100vh" overflow="auto">
-      <Landing />
+      Hello world
     </Center>
   );
 }
