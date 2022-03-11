d478240e30e997d7fb79a3c445f9361f2a81dfde
Keith Radford <khr8771@gmail.com>
2022-03-10 21:06:38 -0800

    fix test

--- src/common/header/containers/__tests__/HeaderContainer.test.tsx
+++ src/common/header/containers/__tests__/HeaderContainer.test.tsx
@@ -5,11 +5,11 @@ import { renderWithSearchAndRouter } from 'lib/utils/jest';
 import { HeaderContainer } from '../HeaderContainer';
 
 describe('HeaderContainer', () => {
-  it('should render the  header', () => {
+  it('should render the header', () => {
     renderWithSearchAndRouter(<HeaderContainer />);
 
     expect(screen.getByTestId('desktop-header')).toBeInTheDocument();
-    expect(screen.getAllByRole('button')).toHaveLength(8);
+    expect(screen.getAllByRole('button')).toHaveLength(5);
     expect(screen.getByRole('combobox')).toBeInTheDocument();
   });
 });
