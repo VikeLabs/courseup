cee90c7b8b805a2a512a5dc8be1411aa740cbf3c
Keith Radford <khr8771@gmail.com>
2022-03-13 15:54:55 -0700

    revert changes

--- src/pages/scheduler/index.tsx
+++ src/pages/scheduler/index.tsx
@@ -1,6 +1,4 @@
-import { Box, Drawer, DrawerBody, DrawerContent, DrawerOverlay, useDisclosure } from '@chakra-ui/react';
 import { useParams } from 'react-router';
-import { useSwipeable } from 'react-swipeable';
 
 import { Term } from 'lib/fetchers';
 
@@ -12,12 +10,6 @@ import { SchedulerContainer } from './containers/SchedulerContainer';
 
 export function Scheduler(): JSX.Element {
   const { term } = useParams();
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const handlers = useSwipeable({
-    trackMouse: true,
-    onSwipedRight: () => (isOpen ? onClose() : onOpen()),
-    onSwipedLeft: () => (isOpen ? onClose() : onOpen()),
-  });
 
   return (
     <Page
@@ -25,20 +17,7 @@ export function Scheduler(): JSX.Element {
       leftSidebar={<Courses term={term as Term} />}
       rightSidebar={<SchedulerSidebar term={term} />}
     >
-      <Box {...handlers} w="100%">
-        {/* {isOpen ? <Courses term={term as Term} /> : <SchedulerContainer />} */}
-        <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerBody>
-              <Box w="100%" h="100%">
-                <Courses term={term as Term} />
-              </Box>
-            </DrawerBody>
-          </DrawerContent>
-        </Drawer>
-        <SchedulerContainer />
-      </Box>
+      <SchedulerContainer />
     </Page>
   );
 }
