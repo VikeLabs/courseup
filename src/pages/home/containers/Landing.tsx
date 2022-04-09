b726529568858b2812bedb4bd96bf25fd1eab5de
Keith Radford <53020925+keithradford@users.noreply.github.com>
2022-04-09 11:06:26 -0700

    feat: update landing page for logo voting (#363)

--- src/pages/home/containers/Landing.tsx
+++ src/pages/home/containers/Landing.tsx
@@ -1,32 +1,65 @@
-import { Image } from '@chakra-ui/image';
-import { Button, Center, VStack } from '@chakra-ui/react';
-import { BsPalette } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
-import { Countdown } from 'pages/contest/components/Countdown';
+import { Image } from '@chakra-ui/image';
+import { Container, Grid, GridItem, Heading, Text } from '@chakra-ui/layout';
+import { Button } from '@chakra-ui/react';
+import { MdHowToVote } from 'react-icons/md';
 
 export function Landing() {
+  const [handIndex, setHandIndex] = useState(0);
+  const hands = ['👋', '👋🏻', '👋🏼', '👋🏽', '👋🏾', '👋🏿'];
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setHandIndex((handIndex + 1) % hands.length);
+    }, 2000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [handIndex, hands.length]);
+
   return (
-    <Center p="5">
-      <VStack h="100%" w="100%">
-        <Image
-          alt="logo contest graphic"
-          src={process.env.PUBLIC_URL + '/assets/contest/logo_design_contest.svg'}
-          maxH="400px"
-          userSelect="none"
-        />
-        <Countdown />
-        <Button
-          mt="1em !important"
-          as={Link}
-          to="/contest?from=landing"
-          colorScheme="pink"
-          rightIcon={<BsPalette />}
-          size="lg"
-        >
-          Click me for more details!
-        </Button>
-      </VStack>
-    </Center>
+    <Grid
+      templateColumns={{ xl: 'repeat(2, 1fr)', lg: 'repeat(1, 1fr)' }}
+      gap={2}
+      m={10}
+      justifyItems="center"
+      alignItems="center"
+      textAlign={{ base: 'center', xl: 'left' }}
+    >
+      <GridItem colSpan={1}>
+        <Container>
+          <Heading fontSize="4.75em" lineHeight="1.25" mb="1.75rem">
+            Logo contest voting
+          </Heading>
+          <Text fontSize="1.6em">
+            Vote for your favourite logo! Click the button below to view submissions and cast your vote.
+          </Text>
+          <Button
+            mt="1em !important"
+            as="a"
+            href="https://docs.google.com/forms/d/e/1FAIpQLSc5EWpm5ItckY1aIvfF8jyrDvmBhRzbRwhM8EtrZ-GKscZgXw/viewform?usp=sf_link"
+            colorScheme="pink"
+            rightIcon={<MdHowToVote />}
+            size="lg"
+            target="_blank"
+          >
+            Vote now!
+          </Button>
+        </Container>
+      </GridItem>
+      <GridItem display={{ base: 'none', xl: 'initial' }} colSpan={1}>
+        <Container>
+          <Image
+            alt="cartoon image of computer"
+            src={process.env.PUBLIC_URL + '/assets/contest/voting.svg'}
+            sx={{
+              filter: 'drop-shadow( 1.5rem 1rem 1.75rem rgba(0, 0, 0, .25) )',
+            }}
+          />
+        </Container>
+      </GridItem>
+    </Grid>
   );
 }
