ad508059e8cfe40af8a6845040e76a97f95663b6
Aomi Jokoji <aomi@uvic.ca>
2022-04-25 12:00:05 +0900

    feat: development usage api endpoint (won't work in prod)

---
+++ pages/api/banner/v9/[term].ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { BannerClient } from '../../../../utils/bannerClient'
+
+const banner = new BannerClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { term } = req.query as { term: string };
+    await banner.getTerms(0, 100)
+    await banner.setTerm(term)
+    const d = await banner.getSearchResultsAll()
+    res.status(200).json(d);
+}
