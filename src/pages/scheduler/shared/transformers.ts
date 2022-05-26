2466a5f13d2e74670ab348d23cf731bf8eede705
Eduardo Szeckir <eszeckirjr@gmail.com>
2022-05-26 13:36:48 -0700

    Simplifying convoluted expression

--- src/pages/scheduler/shared/transformers.ts
+++ src/pages/scheduler/shared/transformers.ts
@@ -20,11 +20,7 @@ export const courseCalEventToResource = (course: CourseCalendarEvent, opacity = 
   dashedBorder: course.dashedBorder,
   sectionCode: course.sectionCode,
   location: course.meetingTime.where,
-  locationAbbreviation:
-    course.meetingTime &&
-    course.meetingTime.buildingAbbreviation &&
-    course.meetingTime.roomNumber &&
-    course.meetingTime.buildingAbbreviation + ' ' + course.meetingTime.roomNumber,
+  locationAbbreviation: course.meetingTime.buildingAbbreviation + ' ' + course.meetingTime.roomNumber,
   opacity,
 });
 
