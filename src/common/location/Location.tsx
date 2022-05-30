cbf7713342cb3d4c80bf79fa0381624a89bde588
Eduardo Szeckir <eszeckirjr@gmail.com>
2022-05-30 10:50:26 -0700

    Creating location component

---
+++ src/common/location/Location.tsx
@@ -0,0 +1,20 @@
+import { Tooltip, useMediaQuery, Text } from '@chakra-ui/react';
+
+export default function Location({
+  short,
+  long,
+  alwaysShort,
+}: {
+  short?: String;
+  long: String;
+  alwaysShort: boolean;
+}): JSX.Element {
+  const [isMobile] = useMediaQuery('(max-width: 1020px)');
+  return short && (alwaysShort || isMobile) ? (
+    <Tooltip label={long}>
+      <Text>{short}</Text>
+    </Tooltip>
+  ) : (
+    <Text>{long}</Text>
+  );
+}
