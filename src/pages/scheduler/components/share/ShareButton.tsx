bdf854d667d025d8c3fb8e8b8bdeaf41067bc2de
Dominique Charlebois <domo.charlebois@gmail.com>
2022-06-06 23:11:25 -0700

    add screenshot footer and screenshot logic

--- src/pages/scheduler/components/share/ShareButton.tsx
+++ src/pages/scheduler/components/share/ShareButton.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 
 import { Button, Icon, IconButton, useDisclosure, useMediaQuery } from '@chakra-ui/react';
+import html2canvas from 'html2canvas';
+import { FiCamera } from 'react-icons/fi';
 import { IoShareOutline } from 'react-icons/io5';
 import { useMatch } from 'react-router';
 
@@ -48,29 +50,84 @@ export function ShareButton({ term, disabled }: { term: Term; disabled: boolean 
     onOpen();
   };
 
+  const handleScreenshot = () => {
+    // target the calendar container exclusively
+    const calendarHTMLElement: HTMLElement = document.getElementsByClassName('rbc-time-view')[0] as HTMLElement;
+    // select the screenshot footer and clone for manipulation
+    const footerHTMLElement: HTMLElement = document.getElementById('screenshotFooter') as HTMLElement;
+    const footerHTMLElementClone = footerHTMLElement.cloneNode(true) as HTMLElement;
+
+    footerHTMLElementClone.style.visibility = 'visible';
+    calendarHTMLElement.appendChild(footerHTMLElementClone);
+
+    // on mobile the screenshot is of the day view
+    // on desktop the screenshot is of the week view
+    html2canvas(calendarHTMLElement, {
+      windowHeight: isMobile ? 1080 : 1080,
+      windowWidth: isMobile ? 360 : 1920,
+    })
+      .then((canvas) => {
+        downloadCalendarScreenshot(canvas.toDataURL(), `${term}_calendar.png`);
+        calendarHTMLElement.removeChild(footerHTMLElementClone);
+      })
+      .then(() => {
+        // open share modal to encourage sharing
+        handleShare();
+      });
+  };
+
+  const downloadCalendarScreenshot = (uri: string, filename: string) => {
+    const downloadLink = document.createElement('a');
+
+    if (typeof downloadLink.download === 'string') {
+      downloadLink.href = uri;
+      downloadLink.download = filename;
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+    } else {
+      window.open(uri);
+    }
+  };
+
   return importPage ? (
     <></>
   ) : (
     <>
       {isMobile ? (
-        <IconButton
-          aria-label="Share timetable"
-          icon={<IoShareOutline />}
-          disabled={disabled}
-          size="sm"
-          colorScheme="purple"
-          onClick={handleShare}
-        />
+        <>
+          <IconButton
+            aria-label="Share timetable"
+            icon={<IoShareOutline />}
+            disabled={disabled}
+            size="sm"
+            colorScheme="purple"
+            onClick={handleShare}
+          />
+          <IconButton
+            icon={<FiCamera />}
+            aria-label="Download screenshot of timetable"
+            size="sm"
+            colorScheme="orange"
+            onClick={handleScreenshot}
+            disabled={disabled}
+          />
+        </>
       ) : (
-        <Button
-          disabled={disabled}
-          size="sm"
-          colorScheme="purple"
-          leftIcon={<Icon as={IoShareOutline} />}
-          onClick={handleShare}
-        >
-          Share
-        </Button>
+        <>
+          <Button
+            disabled={disabled}
+            size="sm"
+            colorScheme="purple"
+            leftIcon={<Icon as={IoShareOutline} />}
+            onClick={handleShare}
+          >
+            Share
+          </Button>
+          <Button size="sm" colorScheme="orange" onClick={handleScreenshot} disabled={disabled}>
+            Screenshot
+          </Button>
+        </>
       )}
       <ShareTimetableModal
         term={term}
