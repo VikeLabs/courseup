386aa6e1943abc356944ee4423b75b64b8a1ec89
Dominique Charlebois <domo.charlebois@gmail.com>
2022-06-06 20:03:14 -0700

    move screenshot to share component

--- src/pages/scheduler/components/Toolbar.tsx
+++ src/pages/scheduler/components/Toolbar.tsx
@@ -1,6 +1,5 @@
-import { CalendarIcon, ChevronLeftIcon, ChevronRightIcon, DownloadIcon } from '@chakra-ui/icons';
+import { ChevronLeftIcon, ChevronRightIcon, DownloadIcon } from '@chakra-ui/icons';
 import { Button, Flex, HStack, IconButton, Text } from '@chakra-ui/react';
-import html2canvas from 'html2canvas';
 import { ToolbarProps } from 'react-big-calendar';
 
 import { Term } from 'lib/fetchers';
@@ -34,67 +33,24 @@ export const CalendarToolBar =
       }
     };
 
-    const handleScreenshot = () => {
-      // target the calendar container exclusively
-      const calendarHTMLElement: HTMLElement = document.getElementsByClassName('rbc-time-view')[0] as HTMLElement;
-
-      // on mobile the screenshot is of the day view
-      // on desktop the screenshot is of the week view
-      html2canvas(calendarHTMLElement, {
-        windowHeight: isMobile ? 780 : 1080,
-        windowWidth: isMobile ? 360 : 1920,
-      }).then((canvas) => {
-        downloadCalendarScreenshot(canvas.toDataURL(), `${term}_calendar.png`);
-      });
-    };
-
-    const downloadCalendarScreenshot = (uri: string, filename: string) => {
-      const downloadLink = document.createElement('a');
-
-      if (typeof downloadLink.download === 'string') {
-        downloadLink.href = uri;
-        downloadLink.download = filename;
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
-      } else {
-        window.open(uri);
-      }
-    };
-
     return (
       <Flex pb="0.5em" justifyContent="space-between" alignItems="center">
         <Text fontSize={{ base: 'xs', sm: 'xl' }}>{label}</Text>
         <HStack pb="0.2em">
           <ShareButton term={term as Term} disabled={!vCalendar} />
           {isMobile ? (
-            <>
-              <IconButton
-                icon={<DownloadIcon />}
-                aria-label="Download timetable"
-                size="sm"
-                colorScheme="blue"
-                onClick={handleDownload}
-                disabled={!vCalendar}
-              />
-              <IconButton
-                icon={<CalendarIcon />}
-                aria-label="Download screenshot of timetable"
-                size="sm"
-                colorScheme="orange"
-                onClick={handleScreenshot}
-                disabled={!vCalendar}
-              />
-            </>
+            <IconButton
+              icon={<DownloadIcon />}
+              aria-label="Download timetable"
+              size="sm"
+              colorScheme="blue"
+              onClick={handleDownload}
+              disabled={!vCalendar}
+            />
           ) : (
-            <>
-              <Button size="sm" colorScheme="blue" onClick={handleDownload} disabled={!vCalendar}>
-                Download
-              </Button>
-              <Button size="sm" colorScheme="orange" onClick={handleScreenshot} disabled={!vCalendar}>
-                Screenshot
-              </Button>
-            </>
+            <Button size="sm" colorScheme="blue" onClick={handleDownload} disabled={!vCalendar}>
+              Download
+            </Button>
           )}
           <Button size="sm" colorScheme="gray" onClick={handleClick()}>
             Today
