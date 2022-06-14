267b9a5edc81c26aed5390c826dc9d9860a63b53
Josho <joshomac505@gmail.com>
2022-06-13 21:34:20 -0700

    Displays a list of all requisites in a single line

--- src/pages/calendar/components/Requisites.tsx
+++ src/pages/calendar/components/Requisites.tsx
@@ -2,22 +2,32 @@ import { KualiCourse, NestedPreCoRequisites } from 'lib/fetchers';
 
 // If stuff is string, return it
 // If not, get string of NestedPreCoRequisites or KualiCourse
-export function myFunction(
-  req: string | NestedPreCoRequisites | KualiCourse
-): string | NestedPreCoRequisites | KualiCourse {
+export function myFunction(req: string | NestedPreCoRequisites | KualiCourse): String {
   if (typeof req === 'string') {
     return req;
+    // It's nested
+  } else if ('reqList' in req) {
+    return nestedReq(req);
+  } else if ('code' in req) {
+    return req.subject + req.code;
   }
-  // } else if (req === 'NestedPreCoRequisites') {
-  //   if (req.reqList) {
-  //     return myFunction(req.reqList[0]);
-  //   } else {
-  //     req.quantity + ' of the following:';
-  //   }
-  // } else if (req === 'KualiCourse') {
-  //   return req.subject;
-  // }
-  return 'Hey ' + req;
+  return 'Failed';
+}
+
+// TODO: Figure out how to display the individual items one by one. Returning just cuts it off and shows once
+// Once that is done, can add a case for when coreq == true
+// Add some stuff for quantity
+// Hyperlinks for the courses
+export function nestedReq(req: NestedPreCoRequisites): String {
+  var i = 0;
+  var out = '';
+  if (req.reqList !== undefined) {
+    while (req.reqList[i] !== undefined) {
+      out += myFunction(req.reqList[i]) + ' ';
+      i += 1;
+    }
+  }
+  return out;
 }
 
 export type RequisiteProp = {
@@ -28,12 +38,21 @@ export type RequisiteProp = {
 export function Requisites({ preAndCorequisites, preOrCorequisites }: RequisiteProp) {
   return (
     <div>
-      {preAndCorequisites?.map((req) => {
-        return myFunction(req);
-      })}
-      {preOrCorequisites?.map((req) => {
-        return myFunction(req);
-      })}
+      <p>Requisites</p>
+      <li>
+        {preAndCorequisites?.map((req) => {
+          if (typeof req === 'string') {
+            return req;
+          }
+          return myFunction(req);
+        })}
+        {preOrCorequisites?.map((req) => {
+          if (typeof req === 'string') {
+            return req;
+          }
+          return myFunction(req);
+        })}
+      </li>
     </div>
   );
 }
