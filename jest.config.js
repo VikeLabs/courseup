0d476410cab611bb5379d2a711d74c337cbab45a
Aomi Jokoji <aomi@uvic.ca>
2022-08-30 17:33:57 -0700

    feat: use nextjs preset for jest test runner

--- jest.config.js
+++ jest.config.js
@@ -1,30 +1,26 @@
-const baseTsConfig = require('./tsconfig.json');
-// Sync object
-/** @type {import('@jest/types').Config.InitialOptions} */
-const config = {
-  verbose: true,
-  preset: 'ts-jest',
-  globals: {
-    'ts-jest': {
-      tsconfig: {
-        ...baseTsConfig.compilerOptions,
-        jsx: 'react-jsx',
-      },
-    },
-  },
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
+// jest.config.js
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  // Add more setup options before each test is run
   testPathIgnorePatterns: ['/node_modules', 'functions'],
-  testRegex: 'src/.*(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+  moduleDirectories: ['node_modules', '<rootDir>/'],
   modulePaths: ['<rootDir>/src/'],
   moduleNameMapper: {
-    '^.+\\.(css|scss)$': '<rootDir>/src/lib/utils/jest/__mocks__/styleMock.js',
     '^swiper.*$': '<rootDir>/src/lib/utils/jest/__mocks__/moduleMock.js',
   },
+  // js-dom setup
+  testEnvironment: 'jest-environment-jsdom',
 };
 
-module.exports = config;
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
