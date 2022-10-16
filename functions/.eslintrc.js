7ca6ccaa693bc2c404a166c5a78683c0cfced5ee
Eduardo Szeckir <eszeckirjr@gmail.com>
2022-10-16 15:23:57 -0700

    Remove eslint fix

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -19,7 +19,6 @@ module.exports = {
   parserOptions: {
     project: ['tsconfig.json', 'tsconfig.dev.json'],
     sourceType: 'module',
-    tsconfigRootDir: __dirname,
   },
   ignorePatterns: [
     '/lib/**/*', // Ignore built files.
