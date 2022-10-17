2dc03d4a23c5deef528e4911d35932918c400097
Eduardo Szeckir <eszeckirjr@gmail.com>
2022-10-17 13:17:26 -0700

    Add sections scripts and tests

---
+++ scripts/upsert/__tests__/sections.test.ts
@@ -0,0 +1,82 @@
+import { upsertSectionsScript } from '../sections';
+import { createTask } from '../../../lib/task';
+import { differenceInMinutes } from 'date-fns';
+
+jest.mock('date-fns', () => ({
+  differenceInMinutes: jest.fn(),
+}));
+
+jest.mock('../../../lib/task', () => ({
+  findLatestTask: jest.fn(),
+  createTask: jest.fn(),
+}));
+
+jest.mock('../../../lib/sections', () => ({
+  upsertSections: jest.fn(),
+}));
+
+jest.mock('../../../lib/banner', () => ({
+  getSearchResults: jest.fn().mockReturnValue({
+    data: [],
+    totalCount: 0,
+    pageMaxSize: 0,
+  }),
+}));
+
+describe('upsert-sections', () => {
+  beforeAll(() => {
+    jest.useFakeTimers('modern');
+    jest.setSystemTime(new Date(2020, 8, 21));
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should run the task every time if before the drop date', async () => {
+    const registrationDate = new Date(2020, 9, 1);
+    const dropDate = new Date(2020, 10, 31);
+    jest.setSystemTime(new Date(2020, 9, 16));
+    await upsertSectionsScript('202009', registrationDate, dropDate);
+    expect(createTask).toBeCalledTimes(1);
+  });
+
+  it('should run the task every time if on drop date', async () => {
+    const registrationDate = new Date(2020, 9, 1);
+    const dropDate = new Date(2020, 10, 31);
+    jest.setSystemTime(new Date(2020, 10, 31));
+    await upsertSectionsScript('202009', registrationDate, dropDate);
+    expect(createTask).toBeCalledTimes(1);
+  });
+
+  describe('run task less frequently if drop date has passed', () => {
+    beforeAll(() => {
+      jest.useFakeTimers('modern');
+      jest.setSystemTime(new Date(2020, 11, 5));
+    });
+
+    afterAll(() => {
+      jest.useRealTimers();
+    });
+
+    it('should run the task if after drop date and courses have not been updated within the last 12 hours', async () => {
+      const registrationDate = new Date(2020, 9, 1);
+      const dropDate = new Date(2020, 10, 31);
+      (differenceInMinutes as jest.Mock).mockReturnValue(730);
+      await upsertSectionsScript('202009', registrationDate, dropDate);
+      expect(createTask).toBeCalledTimes(1);
+    });
+
+    it('should not run the task if after drop date and courses have been updated within the last 12 hours', async () => {
+      const registrationDate = new Date(2020, 9, 1);
+      const dropDate = new Date(2020, 10, 31);
+      (differenceInMinutes as jest.Mock).mockReturnValue(300);
+      await upsertSectionsScript('202009', registrationDate, dropDate);
+      expect(createTask).toBeCalledTimes(0);
+    });
+  });
+});
---
+++ scripts/upsert/sections.ts
@@ -0,0 +1,53 @@
+import { Term } from '../../lib/term';
+import { differenceInMinutes } from 'date-fns';
+import { upsertSections } from '../../lib/sections';
+import { findLatestTask, createTask } from '../../lib/task';
+import { Fetch } from '../../lib/banner/fetch';
+import makeFetchCookie from 'fetch-cookie';
+import { getSearchResults } from '../../lib/banner';
+import { range } from '../../lib/fn';
+import fetch from 'node-fetch';
+
+export const upsertSectionsScript = async (term: string, registrationDay: Date, dropDate: Date) => {
+  const today = new Date();
+
+  const lastUpdated = await findLatestTask(`upsertSections-${term}`);
+
+  const minutesSinceLastUpdate = differenceInMinutes(today, lastUpdated?.startedAt ?? -1);
+
+  // If after drop date and courses have been updated within the last 12 hours, return
+  if (today > dropDate && minutesSinceLastUpdate <= 720) return;
+
+  // Else, run the task
+
+  // Establish a session for each request
+  const fc = makeFetchCookie(fetch) as unknown as Fetch;
+
+  // Get sections
+  const init = await getSearchResults(fc, { term, max: 500 });
+  let sectionsCount = 0;
+  const { totalCount } = init;
+
+  sectionsCount += init.data.length;
+  const offsets = range(init.data.length, totalCount, init.pageMaxSize);
+
+  const data = [...init.data];
+  for (const offset of offsets) {
+    const { data } = await getSearchResults(fc, { term, max: init.pageMaxSize, offset });
+    data.concat(data);
+    sectionsCount += data.length;
+  }
+
+  await createTask(`upsertSections-${term}`, upsertSections(fc, term, data), {});
+};
+
+if (process.env[2] && process.env[3]) {
+  const registrationDate = new Date(process.argv[2]);
+  const dropDate = new Date(process.argv[3]);
+
+  const terms = new Term().sessionTerms();
+
+  terms.forEach(async (term) => {
+    await upsertSectionsScript(term.toString(), registrationDate, dropDate);
+  });
+}
