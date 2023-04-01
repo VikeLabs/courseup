cbca0acf1f8c5c225dee0f451bbcd42e21fab390
Eduardo Szeckir <eszeckirjr@gmail.com>
2023-04-01 14:07:15 -0700

    updating into session terms instead

--- scripts/upsert/courses.ts
+++ scripts/upsert/courses.ts
@@ -5,7 +5,7 @@ import { upsertTermsCourses } from './functions/upsertCourses';
 const registrationDate = process.env[2] ? new Date(process.argv[2]) : new Date();
 const dropDate = process.env[3] ? new Date(process.argv[3]) : new Date();
 const currTerm = new Term();
-const terms = [currTerm, currTerm.next(), currTerm.next().next()];
+const terms = currTerm.sessionTerms();
 
 upsertTermsCourses(terms, registrationDate, dropDate).then(() => {
   console.log('Done');
--- scripts/upsert/sections.ts
+++ scripts/upsert/sections.ts
@@ -5,7 +5,7 @@ import { upsertTermsSections } from './functions/upsertSections';
 const registrationDate = process.env[2] ? new Date(process.argv[2]) : new Date();
 const dropDate = process.env[3] ? new Date(process.argv[3]) : new Date();
 const currTerm = new Term();
-const terms = [currTerm, currTerm.next(), currTerm.next().next()];
+const terms = currTerm.sessionTerms();
 
 upsertTermsSections(terms, registrationDate, dropDate).then(() => {
   console.log('Done');
