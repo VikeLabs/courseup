04d74fc1f48da315c5585bf67a09183b25303d82
Eduardo Szeckir <eszeckirjr@gmail.com>
2023-04-01 15:55:40 -0700

    testing new name

--- scripts/upsert/courses.ts
+++ scripts/upsert/courses.ts
@@ -4,8 +4,8 @@ import { upsertTermsCourses } from './functions/upsertCourses';
 
 const registrationDate = process.env[2] ? new Date(process.argv[2]) : new Date();
 const dropDate = process.env[3] ? new Date(process.argv[3]) : new Date();
-const currTerm = new Term();
-const terms = currTerm.sessionTerms();
+const currentTerm = new Term();
+const terms = currentTerm.sessionTerms();
 
 upsertTermsCourses(terms, registrationDate, dropDate).then(() => {
   console.log('Done');
