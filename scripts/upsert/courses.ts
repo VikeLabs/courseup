88b15f43c6acb5780511e2caa4c8269529519913
Eduardo Szeckir <eszeckirjr@gmail.com>
2023-04-01 17:01:51 -0700

    replacing initial github actions

--- scripts/upsert/courses.ts
+++ scripts/upsert/courses.ts
@@ -2,9 +2,9 @@ import { Term } from '../../lib/term';
 
 import { upsertTermsCourses } from './functions/upsertCourses';
 
-const registrationDate = process.env[2] ? new Date(process.argv[2]) : new Date();
-const dropDate = process.env[3] ? new Date(process.argv[3]) : new Date();
-const currentTerm = new Term();
+const currentTerm = process.env[2] ? Term.fromString(process.argv[2]) : new Term();
+const registrationDate = process.env[3] ? new Date(process.argv[3]) : new Date();
+const dropDate = process.env[4] ? new Date(process.argv[4]) : new Date();
 const terms = currentTerm.sessionTerms();
 
 upsertTermsCourses(terms, registrationDate, dropDate).then(() => {
