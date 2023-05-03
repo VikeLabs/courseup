9119b373c55fde9cc0642a40ecb72e5a8051ba15
Logan Kuzyk <logan@kuzyk.net>
2023-05-02 20:23:50 -0700

    Use useMemo for defaultTerm

--- src/lib/hooks/useTerm.ts
+++ src/lib/hooks/useTerm.ts
@@ -1,3 +1,5 @@
+import { useMemo } from 'react';
+
 import { useParams } from 'react-router';
 
 import { Term } from 'lib/fetchers';
@@ -8,7 +10,7 @@ import useLocalStorage from './storage/useLocalStorage';
 export const useTerm = (): [Term, (term: string) => void] => {
   const { term: termParam } = useParams();
   // The URL parameter should override getCurrentTerm() and the stored term.
-  const defaultTerm = termParam || getCurrentTerm();
+  const defaultTerm = useMemo(() => termParam || getCurrentTerm(), [termParam]);
   const [term, setTerm] = useLocalStorage<string>('user:term', defaultTerm) as [Term, (term: string) => void];
 
   return [term, setTerm];
