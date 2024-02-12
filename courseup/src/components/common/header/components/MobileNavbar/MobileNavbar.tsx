92b648eb1d13da5790aebd448d32e116b2738787
Aaron Cohen <ajccool@icloud.com>
2024-02-12 11:10:57 -0800

    finish up mobile navbar component functionality

--- courseup/src/components/common/header/components/MobileNavbar/MobileNavbar.tsx
+++ courseup/src/components/common/header/components/MobileNavbar/MobileNavbar.tsx
@@ -8,38 +8,42 @@ import { NavButtons } from '../NavButtons';
 import classNames from 'classnames';
 
 export function MobileNavbar({ blurBackground, setBlurBackground }: any): React.ReactNode {
-  const unblurOverride = useRef(false);
+  const menuOverrride = useRef(false);
   const unblurListenerAdded = useRef(false);
 
   const onClick = (): void => {
-    // TODO: make it so that somehow clicking on the menu button once its open closes the dropdown content
     // TODO: also, the outline of the menu still isn't really that visible (that was the point of adding blur)
     // TODO: need to find some way to make it look a little bit better and add some contrast
-    setBlurBackground((prev: Boolean) => !prev);
     const unBlurBackground = () => {
-      if (!unblurOverride.current) {
-        setBlurBackground((prev: Boolean) => !prev);
-        document.removeEventListener('click', unBlurBackground);
-        unblurListenerAdded.current = false;
-      } else {
-        unblurOverride.current = false;
+      if (!blurBackground) {
+        if (!menuOverrride.current) {
+          document.removeEventListener('click', unBlurBackground);
+          setBlurBackground(false);
+        } else {
+          menuOverrride.current = false;
+        }
       }
     };
     if (!unblurListenerAdded.current) {
       document.addEventListener('click', unBlurBackground);
-      unblurListenerAdded.current = true;
+      setBlurBackground(true);
     }
   };
 
   return (
-    <div className="dropdown">
+    <div className={classNames('dropdown', { 'dropdown-open': blurBackground })}>
       <div tabIndex={0} role="button" className="btn btn-ghost md:hidden" onClick={onClick}>
         <DropdownButon active={blurBackground} />
       </div>
       <ul
         tabIndex={0}
-        className={classNames('menu dropdown-content rounded-md bg-base-100 p-0 shadow w-72', 'px-4 py-4 mt-4', 'z-10')}
-        onClick={() => (unblurOverride.current = true)}
+        className={classNames(
+          'menu dropdown-content rounded-md bg-base-100 p-0 shadow w-72',
+          'px-4 py-4 mt-4',
+          'z-10',
+          { hidden: !blurBackground }
+        )}
+        onClick={() => (menuOverrride.current = true)}
       >
         <NavButtons />
       </ul>
