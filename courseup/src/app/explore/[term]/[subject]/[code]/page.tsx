d703b95377984c636f2372a0c4385fb6bd536547
Aaron Cohen <ajccool@icloud.com>
2024-02-28 13:33:55 -0800

    add styling for course display

--- courseup/src/app/explore/[term]/[subject]/[code]/page.tsx
+++ courseup/src/app/explore/[term]/[subject]/[code]/page.tsx
@@ -1,22 +1,44 @@
 import { getCourseBySubjectCode } from '@lib/courses';
 import { getReadableTerm } from '@lib/utils/terms';
+import { children } from 'cheerio/lib/api/traversing';
 
 export default async function ExploreCourse({ params }: { params: { term: string; subject: string; code: string } }) {
   const course = await getCourseBySubjectCode(params.term, params.subject, params.code);
   return (
-    <div>
-      <ul>
-        <h1 className="text-2xl font-bold">
+    <div className="hero min-h-screen place-items-start">
+      <ul className="px-0 py-0 hero-content flex-col items-start">
+        <h1 className="text-5xl font-bold">
           {course.subject}
           {course.code}
         </h1>
-        <h2 className="text-xl font-semibold">{course.title}</h2>
-        <p>{course.description}</p>
-        <p>Lecture: {course.hoursCatalog ? course.hoursCatalog[0].lecture : 0}h</p>
-        <p>Lab: {course.hoursCatalog ? course.hoursCatalog[0].lab : 0}h</p>
-        <p>Tutorial: {course.hoursCatalog ? course.hoursCatalog[0].tutorial : 0}h</p>
-        <p>Credits: {typeof course.credits.value === 'string' ? course.credits.value : course.credits.value.min}</p>
+        <h2 className="text-3xl font-semibold">{course.title}</h2>
+        <HRule />
+        <p className="text-lg">{course.description}</p>
+        <HRule />
+        {/* Badges (Lecture, Lab, Tutorial, Credits) */}
+        <div className="w-full flex flex-row gap-3">
+          <Badge color="primary">Lecture: {course.hoursCatalog ? course.hoursCatalog[0].lecture : 0}h</Badge>
+          <Badge color="secondary">Lab: {course.hoursCatalog ? course.hoursCatalog[0].lab : 0}h</Badge>
+          <Badge color="accent">Tutorial: {course.hoursCatalog ? course.hoursCatalog[0].tutorial : 0}h</Badge>
+        </div>
+        <Badge>
+          Credits: {typeof course.credits.value === 'string' ? course.credits.value : course.credits.value.min}
+        </Badge>
       </ul>
     </div>
   );
+}
+
+function HRule(): JSX.Element {
+  return <div className="w-full h-0.5 rounded-full bg-base-content opacity-75" />;
+}
+
+function Badge({
+  children,
+  color,
+}: {
+  children: Readonly<React.ReactNode>;
+  color?: 'primary' | 'secondary' | 'accent';
+}): React.ReactNode {
+  return <div className={`badge badge-lg ${!!color ? `badge-${color}` : ''} badge-outline`}>{children}</div>;
 }
--- courseup/tailwind.config.ts
+++ courseup/tailwind.config.ts
@@ -15,5 +15,23 @@ const config: Config = {
     },
   },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  //TODO: we need to figure out colors to use for daisyui theming (this one is randomly generated)
+  // daisyui: {
+  //   themes: [
+  //     {
+  //       courseup: {
+  //         primary: '#00b3ff',
+  //         secondary: '#f9aa00',
+  //         accent: '#00e9c2',
+  //         neutral: '#1f1a29',
+  //         'base-100': '#fffdd7',
+  //         info: '#00f2ff',
+  //         success: '#7cb900',
+  //         warning: '#f36500',
+  //         error: '#e70048',
+  //       },
+  //     },
+  //   ],
+  // },
 };
 export default config;
