b44f33614651f0bccbe7bd22e00089a85ce9641f
Aaron Cohen <ajccool@icloud.com>
2024-03-05 11:12:16 -0800

    fix all bugs in ResizeablePanelContainer

--- courseup/src/app/explore/[term]/components/ResizeablePanelContainer/ResizeablePanelContainer.tsx
+++ courseup/src/app/explore/[term]/components/ResizeablePanelContainer/ResizeablePanelContainer.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import classNames from 'classnames';
 import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
 
 export interface ResizeablePanelContainerProps {
@@ -24,12 +25,25 @@ export function ResizeablePanelContainer({
       <Panel order={1} minSize={mainPanelMinSize} maxSize={mainPanelMaxSize} defaultSize={mainPanelDefaultSize}>
         {mainPanelContent}
       </Panel>
-      {/*
-        TODO: make the hover effect have a wider area and add a
-        different color for when its being dragged
-        would also be nice to have a fade transition between the colors
-        */}
-      <PanelResizeHandle className="w-1.5 bg-base-300 hover:bg-base-content active:bg-base-content transition-all duration-200 rounded-full mx-3" />
+      <PanelResizeHandle
+        className="flex"
+        hitAreaMargins={
+          // The mouse is able to enter the "drag area" without triggering css effects
+          // This object adjusts that area as best as possible to minimize the area
+          // In which the mouse is able to enter and NOT trigger effects (I couldn't find
+          // any good documentation describing what coarse and fine actualy do)
+          { coarse: 1, fine: 1 }
+        }
+      >
+        <div className="group w-full h-full flex">
+          <div
+            className={classNames(
+              'w-1.5 bg-base-300 rounded-full mx-3',
+              'group-hover:bg-base-content group-active:bg-primary transition-all duration-200'
+            )}
+          />
+        </div>
+      </PanelResizeHandle>
       <Panel order={2} defaultSize={100 - (mainPanelDefaultSize || mainPanelDefaultSize || 0)}>
         {children}
       </Panel>
