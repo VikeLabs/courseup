148147458a25c87d8ba6c3066cf29f980bf4d64d
Aaron Cohen <ajccool@icloud.com>
2024-06-06 19:09:42 -0700

    add wss folder to nextjs repo lib to fix docker compose issues

---
+++ courseup/lib/wss/server.js
@@ -0,0 +1,14 @@
+const cors = require('cors');
+const express = require('express');
+
+const app = express();
+app.use(cors());
+const PORT = process.env.WSSPORT ?? 1738;
+
+app.get('/', (req, res) => {
+  res.status(200).send({ msg: 'hello world' });
+});
+
+app.listen(PORT, () => {
+  console.log('CourseUp wss backend active on port: ' + PORT);
+});
